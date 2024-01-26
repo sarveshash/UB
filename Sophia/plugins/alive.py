c7a171cd2e729cbc109c702a9ce453a4cc467f7a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 09:28:44 +0530

    Update alive.py

--- Sophia/plugins/alive.py
+++ Sophia/plugins/alive.py
@@ -1,9 +1,9 @@
-from Sophia import *
+from Sophia import HANDLER, OWNER_ID
 from Sophia.__main__ import Sophia
 from pyrogram import *
 import asyncio
 
-@Sophia.on_message(filters.command(["alive", "start"], prefixes=HANDLER) & filters.user(OWNER_ID))
+@Sophia.on_message(filters.command("alive", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def Sophia_Alive(_, message):
     await message.edit("◖⁠⚆⁠ᴥ⁠⚆⁠◗ Loading...")
     await message.edit("Success")
