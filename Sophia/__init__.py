ebfa693ad5c06fca6cee9022a3a438fc8f033d32
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 10:27:30 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -5,8 +5,6 @@ from pyrogram import Client
 SESSION = os.environ.get("SESSION")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
-OWNER_ID = f"{os.environ.get("OWNER_ID")}"
-print(OWNER_ID)
 HANDLER = ["~",".","!","/","$","#"]
 
 Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
