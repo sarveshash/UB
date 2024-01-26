2ae21613006103ac7660678d9888efd141ee9a68
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 23:25:58 +0530

    Update pin_message.py

--- Sophia/plugins/pin_message.py
+++ Sophia/plugins/pin_message.py
@@ -12,9 +12,12 @@ async def pin_message(_, message):
             await Sophia.pin_chat_message(message.chat.id, message.reply_to_message_id)
             await message.edit("Dᴏɴᴇ ✅")
         except Exception as e:
+            if str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""":
+                await message.edit("Mᴀsᴛᴇʀ, ᴡᴇ ɴᴇᴇᴅ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴅᴏ ᴛʜɪs")
+                return
             await message.edit(f"**Sᴏʀʀʏ, ᴍᴀsᴛᴇʀ sᴏᴍᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ᴘʟᴇᴀsᴇ ᴄʜᴇᴀᴄᴋ ᴇʀʀᴏʀs 💔**\n\n`{e}`")
     else:
-        await message.edit("**Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ғᴏʀ ᴘɪɴɴɪɴɢ ɪᴛ ❤️**")
+        await message.edit("**Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ғᴏʀ ᴘɪɴɴɪɴɢ ɪᴛ ❤️**")
 
 
 @Sophia.on_message(filters.command("unpin", prefixes=HANDLER) & filters.user(OWN))
@@ -24,7 +27,10 @@ async def pin_message(_, message):
             await Sophia.unpin_chat_message(message.chat.id, message.reply_to_message_id)
             await message.edit("Dᴏɴᴇ ✅")
         except Exception as e:
+            if str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""":
+                await message.edit("Mᴀsᴛᴇʀ, ᴡᴇ ɴᴇᴇᴅ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴅᴏ ᴛʜɪs")
+                return
             await message.edit(f"**Sᴏʀʀʏ, ᴍᴀsᴛᴇʀ sᴏᴍᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ᴘʟᴇᴀsᴇ ᴄʜᴇᴀᴄᴋ ᴇʀʀᴏʀs 💔**\n\n`{e}`")
     else:
-        await message.edit("**Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ғᴏʀ ᴜɴᴘɪɴɴɪɴɢ ɪᴛ ❤️**")
+        await message.edit("**Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ғᴏʀ ᴜɴᴘɪɴɴɪɴɢ ɪᴛ ❤️**")
         
