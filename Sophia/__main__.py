72d335fdd4812e04c02525dc5e9fdf5d15d890e1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 09:24:43 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,7 +2,7 @@ from Sophia import Sophia
 from pyrogram import Client, filters
 import os
 
-if __name__ == __main__:
+if __name__ == "__main__":
   Sophia.run()
   print("◖⁠⚆⁠ᴥ⁠⚆⁠◗ i'm ded")
   exit()
