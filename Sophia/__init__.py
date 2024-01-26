681e723831ee0288b35afe950b6006ef81d59cc8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 19:50:29 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -6,5 +6,6 @@ SESSION = os.environ.get("SESSION")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 HANDLER = ["~",".","!","/","$","#"]
+MY_VERSION = 0.01
 
 Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
