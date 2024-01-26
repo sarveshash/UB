8faa935bed010b2493c26636c9f28ba23294619c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-26 22:34:58 +0530

    Create pin_message.py

---
+++ Sophia/plugins/pin_message.py
@@ -0,0 +1,10 @@
+from Sophia import HANDLER
+from Sophia.__main__ import Sophia
+from config import OWNER_ID as OWN
+from pyrogram import filters
+import asyncio
+import os
+
+@Sophia.on_message(filters.command("pin", prefix=HANDLER) & filters.user(OWN))
+async def pin_message(_, message):
+    await message.reply("under development")
