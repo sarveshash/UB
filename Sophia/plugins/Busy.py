257796552bccab7ad2a7640290fbb03b748b745c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 00:54:32 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -11,11 +11,10 @@ is_busy = {}
 async def set_busy(_, message):
     is_busy = True
     await message.reply("Master, You are Successfully Set in To Busy Mode.")
-
-@Sophia.on_message()
-async def Say_Offline(_, message):
-    if is_busy == True:
-        if message.from_user.id == OWN:
-            is_busy = False
-            return
-        await message.reply("Sorry My Owner is Currently Offline come later.")
+    @Sophia.on_message()
+    async def Say_Offline(_, message):
+        if is_busy == True:
+            if message.from_user.id == OWN:
+                is_busy = False
+                return
+                await message.reply("Sorry My Owner is Currently Offline come later.")
