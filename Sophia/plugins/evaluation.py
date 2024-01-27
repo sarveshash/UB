0ab037dc0e28a30902995d0313f1673d8946bc9e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-27 12:34:21 +0530

    Update evaluation.py

--- Sophia/plugins/evaluation.py
+++ Sophia/plugins/evaluation.py
@@ -5,12 +5,60 @@ from pyrogram import filters
 import asyncio
 import os
 
-@Sophia.on_message(filters.command(["eval", "e", "python"], prefixes=HANDLER) & filters.user(OWN))
-def evaluation(_, message):
-    if len(message.command) < 2:
-        message.edit("Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴄᴏᴅᴇ ᴛᴏ ʀᴜɴ ɪᴛ. 🥀 ✨")
-        return
-    code = message.text.replace(message.text.split(" ")[0], "")
-    message.edit("Pʀᴏᴄᴇssɪɴɢ...")
-    output = exec(code)
-    message.edit(f"```python\n{output}```")
+@Sophia.on_message(filters.user(OWNER_ID) & filters.command(["eval", "e", "python"], prefixes=HANDLER))
+async def eval(client, message):
+    status_message = await message.reply_text("Pʀᴏᴄᴇssɪɴɢ...")
+    cmd = message.text.split(" ", maxsplit=1)[1]
+
+    reply_to_ = message
+    if message.reply_to_message:
+        reply_to_ = message.reply_to_message
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "<b>EVAL</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>OUTPUT</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n"
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.txt"
+            await reply_to_.reply_document(
+                document=out_file, caption=cmd, disable_notification=True
+            )
+    else:
+        await reply_to_.reply_text(final_output)
+    await status_message.delete()
+
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
