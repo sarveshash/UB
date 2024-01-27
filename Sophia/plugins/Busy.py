877abc7c2755034aea802d704c2b8c9a5a30ce2c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 00:45:13 +0530

    Create Busy.py

---
+++ Sophia/plugins/Busy.py
@@ -0,0 +1,23 @@
+from Sophia import HANDLER
+from Sophia.__main__ import Sophia
+from config import OWNER_ID as OWN
+from pyrogram import filters
+import asyncio
+import os
+
+is_busy = {}
+
+@Sophia.on_message(filters.command(["busy", "afk", "offline"], prefixes=HANDLER) & filters.user(OWN))
+async def set_busy(_, message):
+    global is_busy
+    is_busy = True
+    await message.reply("Master, You are Successfully Set in To Busy Mode.")
+
+@Sophia.on_message()
+async def Say_Offline(_, message):
+    global is_busy
+    if is_busy == True:
+        if message.from_user.id == OWN:
+            is_busy = False
+            return
+        await message.reply("Sorry My Owner is Currently Offline come later.")
