5f9efc8b49a23b72df41941491e06c7b303793d2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-27 23:15:20 +0530

    Update Igonered_Users.py

--- Sophia/plugins/Igonered_Users.py
+++ Sophia/plugins/Igonered_Users.py
@@ -3,22 +3,33 @@ from Sophia.__main__ import Sophia
 from config import OWNER_ID, IGNORED_USERS_ID
 from pyrogram import filters
 import asyncio
-import os
 
-@Sophia.on_message(filters.user(IGNORED_USERS_ID))
-async def Ignored_chat(_, message):
+warning_count = {}
+
+@Sophia.on_message(filters.private & filters.user(IGNORED_USERS_ID))
+async def ignored_chat(_, message):
     Name_get = await Sophia.get_me()
     Name = Name_get.first_name
-    await message.reply_text("I ᴀᴍ ɴᴏᴛ {Name}, ɪ'ᴍ Sᴏᴘʜɪᴀ ᴀ Vɪʀᴜᴛᴀʟ Assɪsᴛᴀɴᴛ. Fᴏʀ {Name},\n ʜᴇ/sʜᴇ Iɢɴᴏʀᴇᴅ ʏᴏᴜ sᴏ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴄʜᴀᴛ ᴡɪᴛʜ ʜɪᴍ/ʜᴇʀ ❌")
-    try:
-        await Sophia.archive_chat(message.chat.id)
-    except Exception as e:
-        print(e)
-        await Sophia.send_message(OWNER_ID, f"Sorry Master, I got Error When Archiving Ignored User. Check Errors Below 💔\n {e}")
-    @Sophia.on_message()
-    async def Warn_to_block(_, message):
-        await message.reply("Sᴏʀʀʏ, ɪ ᴄᴀɴ'ᴛ ᴅᴏ Aɴʏᴛʜɪɴɢ Aғᴛᴇʀ Yᴏᴜ Sᴇɴᴛ ᴍᴇ Aɴᴏᴛʜᴇʀ Msɢ. Bᴄᴢ. ɪ ᴡɪʟʟ Bʟᴏᴄᴋ Yᴏᴜ 💯 (IT'S RULE I CAN'T BREAK IT)")
-        @Sophia.on_message()
-        async def Just_Block_That_Baka(_, message):
-            await message.reply("Nothing Just Block")
-            await Sophia.block_user(message.chat.id)
+    user_id = message.from_user.id
+
+    await message.reply_text(f"I ᴀᴍ ɴᴏᴛ {Name}, ɪ'ᴍ Sᴏᴘʜɪᴀ ᴀ Vɪʀᴜᴛᴀʟ Assɪsᴛᴀɴᴛ. Fᴏʀ {Name},\n ʜᴇ/sʜᴇ Iɢɴᴏʀᴇᴅ ʏᴏᴜ sᴏ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴄʜᴀᴛ ᴡɪᴛʜ ʜɪᴍ/ʜᴇʀ ❌")
+
+    # Check if the user has been warned before
+    if user_id not in warning_count:
+        warning_count[user_id] = 1
+    else:
+        warning_count[user_id] += 1
+
+    # Warn the user
+    if warning_count[user_id] == 1:
+        await Sophia.send_message(user_id, "Sᴏʀʀʏ, ɪ ᴄᴀɴ'ᴛ ᴅᴏ Aɴʏᴛʜɪɴɢ Aғᴛᴇʀ Yᴏᴜ Sᴇɴᴛ ᴍᴇ Aɴᴏᴛʜᴇʀ Msɢ. Bᴄᴢ. ɪ ᴡɪʟʟ Bʟᴏᴄᴋ Yᴏᴜ 💯 (IT'S RULE I CAN'T BREAK IT)")
+    elif warning_count[user_id] == 2:
+        await Sophia.send_message(user_id, "This is your second warning. If you send another message, you will be blocked.")
+    elif warning_count[user_id] >= 3:
+        try:
+            # Archive the chat and block the user
+            await Sophia.archive_chat(message.chat.id)
+            await Sophia.block_user(user_id)
+        except Exception as e:
+            print(e)
+            await Sophia.send_message(OWNER_ID, f"Sorry Master, I got an error when archiving and blocking Ignored User. Check Errors Below 💔\n {e}")
