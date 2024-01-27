4e75fa861c5631de297ddb21a71606c9dfa6c8d7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-27 23:54:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 OWNER_ID = 5965055071 #Replace This To your Owner id
 
 #Sophia will make Them in archive and Sophia will Reply To Them.
-IGNORED_USERS_ID = [6667885638, 6568136732]
+IGNORED_USERS_ID = [6667885638, 5993971291]
 
