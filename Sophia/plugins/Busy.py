b11d79724106ff7218d95a6f7d8a69f033d05bcb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 00:51:13 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -5,19 +5,19 @@ from pyrogram import filters
 import asyncio
 import os
 
-is_busy = {}
+is_busy_ = {}
 
 @Sophia.on_message(filters.command(["busy", "afk", "offline"], prefixes=HANDLER) & filters.user(OWN))
 async def set_busy(_, message):
-    global is_busy
-    is_busy = True
+    global is_busy_
+    is_busy_ = True
     await message.reply("Master, You are Successfully Set in To Busy Mode.")
 
 @Sophia.on_message()
 async def Say_Offline(_, message):
-    global is_busy
-    if is_busy == True:
+    global is_busy_
+    if is_busy_ == True:
         if message.from_user.id == OWN:
-            is_busy = False
+            is_busy_ = False
             return
         await message.reply("Sorry My Owner is Currently Offline come later.")
