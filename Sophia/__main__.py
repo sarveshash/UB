071536e3e357e1e071cb50bd312a9b97eb139c10
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-27 21:05:09 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -1,6 +1,13 @@
 from Sophia import Sophia
 from pyrogram import Client, filters
 import os
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
 
 if __name__ == "__main__":
   Sophia.run()
