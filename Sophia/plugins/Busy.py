b8b02587c49ecb3beab590b6c19db1d254bb4abb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 16:26:01 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -2,6 +2,7 @@ from pyrogram import filters
 from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID as OWN
+from config import IGNORED_USERS_ID
 from Restart import restart_program
 import os
 
@@ -15,6 +16,8 @@ async def set_into_busy(_, message):
     if Busy_stats == True:
         @Sophia.on_message(filters.private & ~filters.user(OWN))
         async def say_master_is_busy(_, message):
+            if message.from_user.id == IGNORED_USERS_ID:
+                return
             await message.reply_text("Huh, My **Master** Currently In **Offline** Can you Come **Later?**")
             @Sophia.on_message(filters.user(OWN))
             async def remove_busy_mode(_, message):
