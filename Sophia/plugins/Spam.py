6ed80ee0b7136053b46bba9c47291fb8cd69398f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 23:21:56 +0530

    Create Spam.py

---
+++ Sophia/plugins/Spam.py
@@ -0,0 +1,29 @@
+from Sophia import HANDLER
+from Sophia.__main__ import Sophia
+from config import OWNER_ID as OWN
+from pyrogram import filters
+import asyncio
+import os
+
+is_spam_running = {}
+spam_stop = {}
+
+@Sophia.on_message(filters.command("spam", prefixes=HANDLER) & filters.user(OWN))
+async def spam(_, message):
+    global is_spam_running, spam_stop
+    if len(message.text.split()) <2:
+          return await message.reply_text("Master, give a input to spam")
+    text = message.text.split(None, 1)[1]
+    is_spam_running = True
+    while spam_stop == True:
+        await Sophia.send_message(message.chat.id, text)
+
+@Sophia.on_message(filters.command(["stopspam", "sspam", "endspam"], prefixes=HANDLER) & filters.user(OWN))
+async def spam_stoper(_, message):
+    global is_spam_running, spam_stop
+    if is_spam_running == True:
+        spam_stop = True
+        is_spam_running = False
+        await message.reply_text("I stopped That spam successfully ✅")
+    else:
+        await message.reply_text('No Spam Currently Running??')
