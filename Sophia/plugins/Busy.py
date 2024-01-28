dea185e862f1e605acb50c8d55ec86cc7d7d2b43
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 21:47:02 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -9,18 +9,28 @@ import re
 
 
 Busy_stats = {}
-
+Does_Reason_Available = {}
+Reason_Of_Busy = {}
 
 @Sophia.on_message(filters.command(["busy", "offline", "afk"], prefixes=HANDLER) & filters.user(OWN))
 async def set_into_busy(_, message):
-    global Busy_stats
-    Busy_stats = True
+    global Busy_stats, Does_Reason_Available, Reason_Of_Busy
+    if len(message.command) < 2:
+        Busy_stats = True
+        Does_Reason_Available = False
+        Reason_Of_Busy = None
+    else:
+        Busy_stats = True
+        Does_Reason_Available = True
+        Reason_Of_Busy = " ".join(message.command[1:])
     await message.reply_text("💖 **Master**, I Set You In Offline Mode I will Reply to everyone who wants to talk you, bye 👋")
     if Busy_stats == True:
         @Sophia.on_message(filters.private & ~filters.user(OWN))
         async def say_master_is_busy(_, message):
             if message.from_user.id == IGNORED_USERS_ID:
                 return
+            if Does_Reason_Available == True:
+                await message.reply_text(f"**Sorry**, My Master is Currently Offline Because: `{Reason_Of_Busy}`")
             await message.reply_text("**Sorry**, My **Master** is Currently In Offline Can you Come Later?")
         @Sophia.on_message(filters.user(OWN))
         async def remove_busy_mode(_, message):
