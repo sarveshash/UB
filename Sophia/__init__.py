8c58e25346f52ef97cabaad2b85a00f1daf6d74f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 10:46:05 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -8,7 +8,5 @@ API_HASH = os.environ.get("API_HASH")
 HANDLER = ["~",".","!","/","$","#"]
 MY_VERSION = 0.01
 
-# START MESSAGE SEND CLIENT OF SOPHIA
-Sophia_StartMSG = Client("Sophia_Start_Message", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
-# NORMAL CLIENT OF SOPHIA
+# MAIN CLIENT OF SOPHIA
 Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
