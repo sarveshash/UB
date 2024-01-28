3d6f1769d2dc0de777501b82c9e657737dcd1123
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 23:57:38 +0530

    Update busy.py

--- Sophia/plugins/busy.py
+++ Sophia/plugins/busy.py
@@ -1,4 +1,5 @@
 from pyrogram import filters
+from Sophia.plugins.Spam import what_is_text as TXT_FROM_SPAM
 from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID as OWN
@@ -38,7 +39,7 @@ async def set_into_busy(_, message):
                 await message.reply_text("**Sorry**, My **Master** is Currently In Offline Can you Come Later?")
         @Sophia.on_message(filters.user(OWN))
         async def remove_busy_mode(_, message):
-            if message.text == "Sᴏʀʀʏ, Yᴏᴜ ᴀʀᴇ ɪɢɴᴏʀᴇᴅ ʙʏ ᴍʏ ʟᴏᴠᴇʟʏ ❤️ Mᴀsᴛᴇʀ, ɪғ ʏᴏᴜ sᴇɴᴅ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴀɢᴀɪɴ ʏᴏᴜ ᴡɪʟʟ ʙᴇ ɢᴇᴛ Bʟᴏᴄᴋᴇᴅ." or message.text == "This is your second warning. If you send another message, you will be blocked." or message.text == "Sorry, You Have Breaked Your Limits that's why I blocked You!":
+            if message.text == "Sᴏʀʀʏ, Yᴏᴜ ᴀʀᴇ ɪɢɴᴏʀᴇᴅ ʙʏ ᴍʏ ʟᴏᴠᴇʟʏ ❤️ Mᴀsᴛᴇʀ, ɪғ ʏᴏᴜ sᴇɴᴅ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴀɢᴀɪɴ ʏᴏᴜ ᴡɪʟʟ ʙᴇ ɢᴇᴛ Bʟᴏᴄᴋᴇᴅ." or message.text == "This is your second warning. If you send another message, you will be blocked." or message.text == "Sorry, You Have Breaked Your Limits that's why I blocked You!" or message.text == TXT_FROM_SPAM:
                 print(" ")
             elif message.text.startswith("Master, I have Been Blocked A user From Ignored"):
                 print(" ")
