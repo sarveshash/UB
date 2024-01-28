75cda8eb126ecaec7abad9a3d01e653f893a43bc
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 16:10:30 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -5,22 +5,18 @@ from config import OWNER_ID as OWN
 from Restart import restart_program
 import os
 
-Busy_stats = False
+Busy_stats = {}
 
 @Sophia.on_message(filters.command(["busy", "offline", "afk"], prefixes=HANDLER) & filters.user(OWN))
 async def set_into_busy(_, message):
     global Busy_stats
     Busy_stats = True
     await message.reply_text("Master, I set you In Busy Mode!")
-
-if Busy_stats == True:
-    @Sophia.on_message(filters.private & ~filters.user(OWN))
-    async def say_master_is_busy(_, message):
-        await message.reply_text("Sorry My Master Is Currently Busy")
-        return
-
-if Busy_stats == True:
-    @Sophia.on_message(filters.user(OWN))
-    async def remove_busy_mode(_, message):
-        await message.reply_text("I have Removed Your Offline Mode")
-        await restart_program()
+    if Busy_stats == True:
+        @Sophia.on_message(filters.private & ~filters.user(OWN))
+        async def say_master_is_busy(_, message):
+            await message.reply_text("Sorry My Master Is Currently Busy")
+            @Sophia.on_message(filters.user(OWN))
+            async def remove_busy_mode(_, message):
+                await message.reply_text("I have Removed Your Offline Mode")
+                await restart_program()
