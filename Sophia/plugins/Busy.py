e34ce3c7d52c2cb7a135a383860431d76af059f9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 14:17:47 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -3,6 +3,7 @@ from Sophia.__main__ import Sophia
 from config import OWNER_ID as OWN
 from pyrogram import filters
 import asyncio
+from Restart import restart_program
 import os
 
 Busy_stats = False
@@ -12,15 +13,17 @@ async def Set_into_busy(_, message):
     global Busy_stats
     Busy_stats = True
     await message.reply_text("Master, I set you In Busy Mode!")
+    
+@Sophia.on_message(filters.user(OWN))
+async def Remove_busy_Mode(_, message):
+    global Busy_stats
+    if Busy_stats == True:
+        await message.reply_text("I have Removed Your Offline Mode")
+        await restart_program()
 
 @Sophia.on_message(~filters.user(OWN))
 async def Say_master_is_busy(_, message):
     global Busy_stats
     if Busy_stats == True:
         await message.reply_text("Sorry My Master Is Current Busy")
-if Busy_stats == True:
-    @Sophia.on_message(filters.user(OWN))
-    async def Remove_busy_Mode(_, message):
-        global Busy_stats
-        Busy_stats = False
-        await message.reply_text("I have Removed Your Offline Mode")
+        
