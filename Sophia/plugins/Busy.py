60a48ecd1fc6f94625c04ff939d49a8cf45af1b5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 16:38:04 +0530

    Update Busy.py

--- Sophia/plugins/Busy.py
+++ Sophia/plugins/Busy.py
@@ -5,9 +5,12 @@ from config import OWNER_ID as OWN
 from config import IGNORED_USERS_ID
 from Restart import restart_program
 import os
+import re
 
 Busy_stats = {}
 
+OWN_USR = "Otazuki"
+pattern = r"\b(?:{})\b".format('|'.join(['{}(?:{})?'.format(re.escape(word), '[a-zA-Z]*' * (len(word)-1)) for word in OWN_USR]))
 @Sophia.on_message(filters.command(["busy", "offline", "afk"], prefixes=HANDLER) & filters.user(OWN))
 async def set_into_busy(_, message):
     global Busy_stats
@@ -19,7 +22,12 @@ async def set_into_busy(_, message):
             if message.from_user.id == IGNORED_USERS_ID:
                 return
             await message.reply_text("Huh, My **Master** Currently In **Offline** Can you Come **Later?**")
-            @Sophia.on_message(filters.user(OWN))
-            async def remove_busy_mode(_, message):
-                await Sophia.send_message(OWN, "[INFO] you no longer in Offline mode")
-                await restart_program()
+            @Sophia.on_message(filters.text & filters.regex(pattern, re.IGNORECASE) & ~filters.user(OWN) & filters.group))
+            async def Group_say_master_offline(_, message):
+                if message.from_user.id == IGNORED_USERS_ID:
+                    return
+                await message.reply_text("Huh, My **Master** Currently In **Offline** Can you Come **Later?**")
+                @Sophia.on_message(filters.user(OWN))
+                async def remove_busy_mode(_, message):
+                    await Sophia.send_message(OWN, "[INFO] you no longer in Offline mode")
+                    await restart_program()
