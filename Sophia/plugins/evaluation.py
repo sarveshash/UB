59eb29a5348b46327434aaa8ef2c4921389706db
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-28 22:58:48 +0530

    Update evaluation.py

--- Sophia/plugins/evaluation.py
+++ Sophia/plugins/evaluation.py
@@ -10,8 +10,8 @@ import os
 
 @Sophia.on_message(filters.user(OWNER_ID) & filters.command(["eval", "e", "python"], prefixes=HANDLER))
 async def eval(client, message):
-    status_message = await message.reply_text("Pʀᴏᴄᴇssɪɴɢ...")
-    cmd = " ".join(message.command[1:])
+    status_message = await message.reply_text("`Processing...`")
+    cmd = message.text.split(None, 1)[1]
 
     reply_to_ = message
     if message.reply_to_message:
