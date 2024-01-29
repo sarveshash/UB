b31d8a1445e569e2f2eea4faa5b2dfc6c7b3b78a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-29 14:13:43 +0530

    Update purge.py

--- Sophia/plugins/purge.py
+++ Sophia/plugins/purge.py
@@ -36,4 +36,5 @@ async def purge(_, m):
         count_del_msg = len(message_ids)
 
         z = await m.reply_text(text=f"Successfully Deleted **{count_del_msg}** Messages...")
+        return
     await m.reply_text("Master, Please Reply to a message to start purge!")
