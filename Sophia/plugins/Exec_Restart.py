8ceb75e583d87c402d977d05ef68e4e1545ed1c4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-29 19:30:19 +0530

    Update Exec_Restart.py

--- Sophia/plugins/Exec_Restart.py
+++ Sophia/plugins/Exec_Restart.py
@@ -2,11 +2,15 @@ from Restart import restart_program
 from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID as OWN
+from config import Do_you_need_warnings
 from pyrogram import filters
 import asyncio
 import os
 
 @Sophia.on_message(filters.command("restart", prefixes=HANDLER) & filters.user(OWN))
 async def Call_Restart_Func(_, message):
-    await message.edit("`Restarting Sophia....`")
-    await restart_program()
+    Restart_Msg = await message.edit("`Restarting Sophia....`")
+    if Do_you_need_warnings == True:
+        await Restart_Msg.edit("**⚠️ Warning ⚠️** `This Will Restart All UserBot Process And You cannot cancel it now ❌`")
+    else:
+        await restart_program()
