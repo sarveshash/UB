3d696bfba3c9a4258e26947e12c721e119a2f2ec
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 13:58:33 +0530

    Create logs.py

---
+++ Sophia/plugins/logs.py
@@ -0,0 +1,29 @@
+import io
+from pyrogram import *
+from Sophia.__main__ import Sophia as bot
+from Sophia import HANDLER
+from config import OWNER_ID
+import traceback
+from subprocess import getoutput as run
+from pyrogram.enums import ChatAction
+
+@bot.on_message(filters.command(["logs", "log"], prefixes=HANDLER) & filters.user(OWNER_ID))
+async def logs(_, message):
+    run_logs = run("tail log.txt")
+    text = await message.reply_text("`Getting logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    await message.reply_text(f"```shell\n{run_logs}```")
+    await text.delete()
+
+
+@bot.on_message(filters.command(["flogs", "flog"]))
+async def logs(_, message):
+    run_logs = run("cat log.txt")
+    text = await message.reply_text("`Sending Full logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
+    with io.BytesIO(str.encode(run_logs)) as logs:
+        logs.name = "log.txt"
+        await message.reply_document(
+            document=logs,
+        )
+    await text.delete()
