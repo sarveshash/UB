7c453bae2ac618014066b8e6ec5e6860f26e6d5d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 23:42:27 +0530

    Update help.py

--- Sophia/plugins/help.py
+++ Sophia/plugins/help.py
@@ -16,7 +16,7 @@ async def sudohelp(_, message):
         print("")
     else:
         return
-    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» .ping - `Get UserBot ping`\n-» .eval - `Run python codes`\n-» .sh - `Run bash commands`\n-» .log - `Get logs`\n.shelp - `Get This`\n\n**Powered By**: @Hyper_Speed0™")
+    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» .ping - `Get UserBot ping`\n-» .eval - `Run python codes`\n-» .sh - `Run bash commands`\n-» .log - `Get logs`\n-» .shelp - `Get This`\n\n**Powered By**: @Hyper_Speed0™")
 # Warning We just take Reference of text from ZaidUserbot
 # And we don't created that "ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı" its from zaiduserbot Others are reference only
 # If zaid seeing this if have problem contact me t.me/Otazuki I will change it!.
