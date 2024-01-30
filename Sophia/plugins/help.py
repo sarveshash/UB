fa1163eabbaf7fe487aa5a6a03a238af063b2823
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 17:01:13 +0530

    Update help.py

--- Sophia/plugins/help.py
+++ Sophia/plugins/help.py
@@ -7,4 +7,4 @@ import os
 
 @Sophia.on_message(filters.command("help", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def help(_, message):
-    await message.reply("ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» Cʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ.\n-» **Join**: @Hyper_Speed0©")
+    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» Cʟɪᴄᴋ [ʜᴇʀᴇ](http://telegra.ph/Sophia-Commands-01-30) ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ.\n-» **Join**: @Hyper_Speed0©")
