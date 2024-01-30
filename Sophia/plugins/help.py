a6604ff13a26ed4d8b91c6b856ef92dbad1402c5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 17:04:45 +0530

    Update help.py

--- Sophia/plugins/help.py
+++ Sophia/plugins/help.py
@@ -8,3 +8,6 @@ import os
 @Sophia.on_message(filters.command("help", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def help(_, message):
     await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» Cʟɪᴄᴋ [ʜᴇʀᴇ](http://telegra.ph/Sophia-Commands-01-30) ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ.\n-» **Join**: @Hyper_Speed0©")
+# Warning We just take Reference of text from ZaidUserbot
+# And we don't created that "ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı" Others Or reference only
+# If zaid seeing this if have problem contact me t.me/Otazuki I will change it!.
