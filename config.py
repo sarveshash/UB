536917b90e71256e3829423146cca9d53d701352
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 22:04:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ OWNER_ID = 5965055071 # Replace This To your Owner id
 Do_you_need_warnings = False
 # Sophia will make Them in archive and Sophia will Reply To Them. and block them
 IGNORED_USERS_ID = [5215360022, 5690711835, 1666544436, 5696053228, 2043144248]
-VIP_USERS_ID = [5015417782, 6568136732]
+SUDO_USERS_ID = [5015417782, 6568136732]
 
