7db6aec6841695da3673cf12b597a0af0f1674d6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 23:33:12 +0530

    Update help.py

--- Sophia/plugins/help.py
+++ Sophia/plugins/help.py
@@ -1,13 +1,22 @@
 from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID
+from config import SUDO_USERS_ID
 from pyrogram import filters
 import asyncio
 import os
 
 @Sophia.on_message(filters.command("help", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def help(_, message):
-    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» Cʟɪᴄᴋ [ʜᴇʀᴇ](http://graph.org/Sophia-Commands-01-30) ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ.\n-» **Join**: @Hyper_Speed0©")
+    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» Cʟɪᴄᴋ [ʜᴇʀᴇ](http://graph.org/Sophia-Commands-01-30) ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ.\n-» **Join**: @Hyper_Speed0™")
+
+@Sophia.on_message(filters.command("sudohelp", prefixes=HANDLER))
+async def help(_, message):
+    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO_USERS_ID:
+        print("")
+    else:
+        return
+    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» .ping - Get UserBot ping\n.eval - Run python codes\n.sh - Run bash commands\n.log - Get logs\n\n**Powered By**: @Hyper_Speed0™")
 # Warning We just take Reference of text from ZaidUserbot
 # And we don't created that "ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı" its from zaiduserbot Others are reference only
 # If zaid seeing this if have problem contact me t.me/Otazuki I will change it!.
