ff6d80e838c7db7413cf5423edd07a55a76b83fd
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-30 19:34:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 OWNER_ID = 5965055071 # Replace This To your Owner id
 # You will Get some Warning messages Using by some Commands if You don't Need Warning message Changed "False" And If you Need Change It "True" Don't add this → ""
-Do_you_need_warnings = True
+Do_you_need_warnings = False
 # Sophia will make Them in archive and Sophia will Reply To Them. and block them
 IGNORED_USERS_ID = [5215360022, 5690711835, 1666544436, 5696053228, 2043144248]
 
