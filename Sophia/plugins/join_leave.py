9f01c5d234eeebf5b6ae4be526a74962e37fd272
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-31 09:14:48 +0530

    Update join_leave.py

--- Sophia/plugins/join_leave.py
+++ Sophia/plugins/join_leave.py
@@ -3,13 +3,24 @@ from config import OWNER_ID
 from Sophia import HANDLER
 from Sophia import Sophia
 
+left_chat_id = {}
 
 @Sophia.on_message(filters.command("join", prefixes=HANDLER) & filters.user(OWNER_ID))
 def join_chat(_, m):
+    global left_chat_id
     if len(m.command) < 2:
         m.reply_text("Master, Give a Group Username or ID to Join")
         return
     link = m.text.split(" ")[1]
+    if link.startswith("Back") or link.startswith("back"):
+        if left_chat_id.startswith("-"):
+            Sophia.join_chat(left_chat_id)
+            chat = Sophia.get_chat(left_chat_id)
+            name = chat.title
+            Sophia.send_message(left_chat_id, f"Successfully Joined in {name}.")
+            return
+        else:
+            message.reply("No chats we left in recently.")
     Sophia.join_chat(link)
     chat = Sophia.get_chat(link)
     name = chat.title
@@ -18,11 +29,13 @@ def join_chat(_, m):
 
 @Sophia.on_message(filters.command("leave", prefixes=HANDLER) & filters.user(OWNER_ID))
 def leave_chat(_, m):
+    global left_chat_id
     if len(m.command) < 2:
         m.reply_text("Master, Give Group username or id to leave it.")
         return
     link = m.text.split(" ")[1]
     if link.startswith("Here") or link.startswith("here"):
+        left_chat_id = m.chat.id
         Sophia.leave_chat(m.chat.id)
         chat = Sophia.get_chat(m.chat.id)
         name = chat.title
