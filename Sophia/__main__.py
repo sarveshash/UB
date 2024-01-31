c9bb0ab9c583487584616464005513d244957902
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-31 13:36:28 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,7 +2,6 @@ from Sophia import *
 from pyrogram import Client, filters
 import os
 import logging
-from config import ACCESS_CODE, ACCESS_PIN
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
