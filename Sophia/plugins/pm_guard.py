b628e4368e51239907c8ca958aed733947f5990e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-11 09:43:39 +0530

    Update pm_guard.py

--- Sophia/plugins/pm_guard.py
+++ Sophia/plugins/pm_guard.py
@@ -1,12 +1,23 @@
-"""from Sophia import HANDLER
+from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID
 from pyrogram import filters
 import asyncio
 import os
 
-@Sophia.on_message(filters.command("", prefixes=HANDLER) & filters.user(OWNER_ID))
-async def set_pm_guard(_, message):"""
+is_pm_block_enabled = False
+approved_users = []
+users_warngings_count = {}
 
+
+@Sophia.on_message(filters.command("pmblock", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def set_pm_guard(_, message):
+    global is_pm_block_enabled
+    if is_pm_block_enabled:
+        is_pm_block_enabled = False
+    else:
+        await message.reply('Feature coming soon')
+        return
+        
 
 # Incompleted
