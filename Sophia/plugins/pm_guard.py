5b154be6d7fa5c7d53c4dc2532c18d72bdf4675f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-11 09:34:51 +0530

    Create pm_guard.py

---
+++ Sophia/plugins/pm_guard.py
@@ -0,0 +1,12 @@
+"""from Sophia import HANDLER
+from Sophia.__main__ import Sophia
+from config import OWNER_ID
+from pyrogram import filters
+import asyncio
+import os
+
+@Sophia.on_message(filters.command("", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def set_pm_guard(_, message):"""
+
+
+# Incompleted
