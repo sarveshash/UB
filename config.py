745c5fffe53cb3ecac5b2360039a311cd9133828
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-11 10:47:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ OWNER_ID = 5965055071 # Replace This To your Owner id
 Do_you_need_warnings = False
 # Sophia will make Them in archive and Sophia will Reply To Them. and block them
 IGNORED_USERS_ID = [5690711835, 1666544436, 5696053228, 2043144248]
+# Sudo Users Id Sudo Users Can Use Some Commands.
 SUDO_USERS_ID = [5015417782, 6568136732]
+# Add Users Id Who Never Want to get Warning message in pmblock.
+Always_Approved_Users_From_Pmblock = []
