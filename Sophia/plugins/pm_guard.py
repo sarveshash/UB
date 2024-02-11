f5257c289ce8b5f93f09fc426fa94831cd4eeb26
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-11 13:07:41 +0530

    Update pm_guard.py

--- Sophia/plugins/pm_guard.py
+++ Sophia/plugins/pm_guard.py
@@ -24,6 +24,9 @@ async def set_pm_guard(_, message):
 @Sophia.on_message(filters.command(['a', 'approve'], prefixes=HANDLER) & filters.user(OWNER_ID))
 async def Approve_user(_, message):
     if is_pm_block_enabled:
+        if message.chat.id.startswith("-"):
+            await message.reply("Only Works On Private Chats.")
+            return
         await message.reply("coming soon")
 
 @Sophia.on_message(~filters.user(OWNER_ID))
