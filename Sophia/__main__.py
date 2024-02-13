ca198365466f80756238fe22ea5b912cf5e44295
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-13 19:36:31 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -4,19 +4,11 @@ import os
 import logging
 import pyrogram
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-# Thanks To KoraXD for Giving Multiple Clients Run code
-async def run_clients():
-    await Sophia.start()
-    await pm_guard_client.start()
-    await pyrogram.idle()
-    
-
-
 if __name__ == "__main__":
-    Sophia.loop.run_until_complete(run_clients())
+    ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
+    if ACCESS == "oTaZUki004nandhaiSgeY":
+        Sophia.run()
+    else:
+        raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
+        exit()
+    
