99e96f85e0511e33ee1f0e5528639005793502f7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-14 05:43:19 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -4,6 +4,12 @@ import os
 import logging
 import pyrogram
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
 if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
