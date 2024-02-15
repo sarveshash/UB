71524725ba61002a9a3db3aa9ce773b31b51609a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-15 10:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
-OWNER_ID = 5965055071 # Replace This To your Owner id
-# You will Get some Warning messages Using by some Commands if You don't Need Warning message Changed "False" And If you Need Change It "True" Don't add this → ""
-Do_you_need_warnings = False
-# Sophia will make Them in archive and Sophia will Reply To Them. and block them
-IGNORED_USERS_ID = [5690711835, 1666544436, 5696053228, 2043144248]
-# Sudo Users Id Sudo Users Can Use Some Commands.
-SUDO_USERS_ID = [5015417782, 6568136732]
-# Add Users Id Who Never Want to get Warning message in pmblock.
-Always_Approved_Users_From_Pmblock = []
+"""
+⚠️ WARNING ⚠️
+
+If you don't know how to use this
+Don't add Something Wrong Here Otherwise this repo won't work, Fill Everything using Instructions from readme.md file.
+If you got problem KINDLY ASK IN SUPPORT CHAT @FUTURECITY005 OR DM ME @OTAZUKI
+I hope you will fill Everything without Any problems
+"""
+
+# REQUIRED THINGS
+OWNER_ID = 5965055071 # Replace this to your Actual Owner's I'd
+
+# SETTINGS 
+Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
+IGNORED_USERS_ID = [5690711835, 1666544436, 5696053228, 2043144248] # Add the person id who you want Ignore.
+SUDO_USERS_ID = [5015417782, 6568136732] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
+Always_Approved_Users_From_Pmblock = [] # Add Users Id Who Never Want to get Warning message in pmblock.
