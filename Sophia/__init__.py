f4de1895e8cf4b032b2041ef363ba482c6891b36
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-16 18:34:46 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -15,6 +15,20 @@ Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HA
 # DATABASE CLIENT OF SOPHIA
 Database = Client("Database", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
 
+# DATABASE CACHE FILE TOTAL CODE
+
+DATABASE_CACHE_CODE = """
+approved_users = []
+is_pm_block_enabled = False
+maximum_message_count = 0
+
+
+Reason_Of_Busy = {}
+Does_Reason_Available = {}
+approved_users = {}
+Busy_stats = {}
+"""
+
 # BETA ACCESS KEY SECTION
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
