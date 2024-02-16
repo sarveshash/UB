3d4c5d3334765dbdccc9da42234867edc7f506b2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-16 21:00:48 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -30,8 +30,7 @@ async def run_clients():
         with open(file_path, 'r') as file:
             content = file.read()
     except Exception as e:
-        print(e)
-        await app.send_document(DATABASE_GROUP_ID, "BQACAgUAAx0CbtjjywACeVFlz3aBLE2v1n5yNAk_3hXisF4azwACWRAAAsrIeFYIS0o5eQ56sh4E", caption="#CACHE_FILE")
+        await Database.send_document(DATABASE_GROUP_ID, "BQACAgUAAx0CbtjjywACeVFlz3aBLE2v1n5yNAk_3hXisF4azwACWRAAAsrIeFYIS0o5eQ56sh4E", caption="#CACHE_FILE")
         await restart_program()
     await Sophia.start()
     await pyrogram.idle()
