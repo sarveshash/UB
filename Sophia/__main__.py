9c24135a1d528016fb1f698d51aef2015002bf76
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-16 13:50:30 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -9,11 +9,18 @@ logging.basicConfig(
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
+async def run_clients():
+    await Database.start()
+    app = Database
+    async for message in app.search_messages(LOGS, query="#CACHE_FILE", limit=1):
+        await Database.download_media(message.document.file_id, file_name="Data.txt")
+        await Sophia.start()
+        await pyrogram.idle()
 
 if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
-        Sophia.run()
+        Sophia.naruto.loop.run_until_complete(run_clients())
     else:
         raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
         exit()
