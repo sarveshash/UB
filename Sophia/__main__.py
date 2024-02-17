a85c82ce523c8f3e4ef2083f6e92f1b4178979e0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-17 11:32:34 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -22,6 +22,10 @@ async def run_clients():
     app = Database
     await app.send_message(DATABASE_GROUP_ID, "Sophia started")
     async for message in app.search_messages(DATABASE_GROUP_ID, query="#CACHE_FILE", limit=1):
+        if message.text == "":
+            await Sophia.send_document(DATABASE_GROUP_ID, f"{PWD}Backup_Data.py", caption="#CACHE_FILE")
+            await restart_program()
+            return
         try:
             await Database.download_media(message.document.file_id, file_name=file_path)
         except Exception as e:
@@ -34,9 +38,6 @@ if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
         Sophia.loop.run_until_complete(run_clients())
-        if not os.path.isfile(file_path):
-            Sophia.send_document(DATABASE_GROUP_ID, f"/{PWD}Backup_Data.py", caption="#CACHE_FILE")
-            restart_program()
     else:
         raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
         exit()
