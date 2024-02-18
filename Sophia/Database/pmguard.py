d04cc0a92b5787fb8f7e7b8c5fe3fa6075e5cac2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-18 23:54:35 +0530

    Update pmguard.py

--- Sophia/Database/pmguard.py
+++ Sophia/Database/pmguard.py
@@ -11,8 +11,14 @@ async def UNSET_PM_GUARD():
     await db.update_one({"_id": 1}, {"$set": {"status": False}})
 
 async def WARNING_COUNT_GET(user_id):
-  
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return None
+    else:
+        value = Find[user_id]
+        return value
 async def WARNING_COUNT_REMOVE(user_id):
-
-async def WARNING_COUNT_ADD(user_id):
-  
+    await db.update_one({"_id": 1}, {"$set": {"user_id": 0}})
+async def WARNING_COUNT_ADD(user_id, count):
+    doc = {"_id": 1, f"{user_id}": count}
+    await db.insert_one(doc)
