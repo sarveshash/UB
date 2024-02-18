f158d1db37dd6c824910484933a58fb2666cdc0a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-18 18:00:41 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -17,6 +17,10 @@ MY_VERSION = 0.00219
 # MAIN CLIENT OF SOPHIA
 Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
 
+# DATABASE OF SOPHIA
+MONGO_DB = MongoClient(MONGO_DB_URI) # Special Thanks To KoraXD For Giving This Codes!!
+DB = MONGO_DB.SOPHIA
+DATABASE = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
 
 # BETA ACCESS KEY SECTION
 
