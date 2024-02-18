f5616ece0f044dcce97136b676ae80eddeff7ca1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-18 22:31:41 +0530

    Update afk.py

--- Sophia/Database/afk.py
+++ Sophia/Database/afk.py
@@ -6,3 +6,14 @@ db = DATABASE["afk"]
 async def SET_AFK(time, reason):
     doc = {"_id": 1, "stats": True, "time": time, "reason": reason}
     await db.insert_one(doc)
+
+async def UNSET_AFK():
+    await db.update_one({"_id": 1}, {$set: {stats: False, "time": None, "reason": None}})
+
+async def GET_AFK():
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return False
+    else:
+        stats = Find["stats"]
+        return stats
