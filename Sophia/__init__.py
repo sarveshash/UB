8544599478e7d9c3a7b1d6cdcdca0df21848cdf3
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-18 17:51:27 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pyrogram import Client
+from config import MONGO_DB_URI
 
 SESSION = os.environ.get("SESSION")
 API_ID = os.environ.get("API_ID")
@@ -12,22 +13,7 @@ MY_VERSION = 0.01
 
 # MAIN CLIENT OF SOPHIA
 Sophia = Client("Sophia", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
-# DATABASE CLIENT OF SOPHIA
-Database = Client("Database", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Sophia/plugins"))
 
-# DATABASE CACHE FILE TOTAL CODE
-
-DATABASE_CACHE_CODE = """
-approved_users = []
-is_pm_block_enabled = False
-maximum_message_count = 0
-
-
-Reason_Of_Busy = {}
-Does_Reason_Available = {}
-approved_users = {}
-Busy_stats = {}
-"""
 
 # BETA ACCESS KEY SECTION
 
