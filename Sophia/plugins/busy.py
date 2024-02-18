d9aa381836df32dfe9c6bff768d078f7e073281f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 00:37:34 +0530

    Update busy.py

--- Sophia/plugins/busy.py
+++ Sophia/plugins/busy.py
@@ -11,7 +11,7 @@ from Sophia.Database.afk import *
 
 @Sophia.on_message(filters.command(["busy", "offline", "afk"], prefixes=HANDLER) & filters.user(OWN))
 async def set_into_busy(_, message):
-    Busy_time = 0
+    Busy_time = {}
     if len(message.command) < 2:
         Busy_time['start'] = time()
         await SET_AFK(Busy_time, None)
