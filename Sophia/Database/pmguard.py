f8936bea55b25d1cc8b420fb9b68e1a46d644419
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 10:18:42 +0530

    Update pmguard.py

--- Sophia/Database/pmguard.py
+++ Sophia/Database/pmguard.py
@@ -3,12 +3,31 @@ import asyncio
 
 db = DATABASE["PM_GUARD"]
 
-async def SET_PM_GUARD():
-    doc = {"_id": 1, "status": True}
-    await db.insert_one(doc)
+async def SET_PM_GUARD(maximum_warn_count):
+    doc = {"_id": 1, "status": True, "warn_count": maximum_warn_count}
+    try:
+        await db.insert_one(doc)
+    except Exception:
+        await db.update_one({"_id": 1}, {"$set": {"status": True, "warn_count": maximum_warn_count}})
 
+async def GET_PM_GUARD():
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return False
+    else:
+        value = Find["status"]
+        return value
+
+async def GET_WARNING_COUNT():
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return None
+    else:
+        value = Find["warn_count"]
+        return value
+    
 async def UNSET_PM_GUARD():
-    await db.update_one({"_id": 1}, {"$set": {"status": False}})
+    await db.update_one({"_id": 1}, {"$set": {"status": False, "warn_count": None}})
 
 async def WARNING_COUNT_GET(user_id):
     Find = await db.find_one({"_id": 1})
@@ -18,7 +37,7 @@ async def WARNING_COUNT_GET(user_id):
         value = Find[user_id]
         return value
 async def WARNING_COUNT_REMOVE(user_id):
-    await db.update_one({"_id": 1}, {"$set": {"user_id": 0}})
+    await db.update_one({"_id": 1}, {"$set": {f"{user_id}": 0}})
 async def WARNING_COUNT_ADD(user_id, count):
     doc = {"_id": 1, f"{user_id}": count}
     await db.insert_one(doc)
