e736ccac8dddc2cf487a237228505108465a2afd
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 18:12:21 +0530

    Update Games.py

--- Sophia/Database/Games.py
+++ Sophia/Database/Games.py
@@ -1,1 +1,19 @@
-# PROJECT GAMES WILL START SOON
+from Sophia import GAME_DATABASE
+import asyncio
+
+db = GAME_DATABASE["Games"]
+
+async def ADD_COINS(user_id, coins):
+    doc = {"_id": 1, f"{user_id}": coins}
+    try:
+        await db.insert_one(doc)
+    except Exception:
+        await db.update_one({"_id": 1}, {"$set": {f"{user_id}": coins}})
+        
+async def GET_USER_COINS(user_id):
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return None
+    else:
+        value = Find[f"{user_id}"]
+        return value
