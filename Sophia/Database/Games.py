52a63ac4a4dd2b2db157a3b262cdf21cc1a49ca7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 18:21:49 +0530

    Update Games.py

--- Sophia/Database/Games.py
+++ Sophia/Database/Games.py
@@ -3,15 +3,32 @@ import asyncio
 
 db = GAME_DATABASE["Games"]
 
-async def ADD_COINS(user_id, coins):
-    doc = {"_id": 1, f"{user_id}": coins}
+async def GET_AVAILABLE_USERS(user_id):
+    Find = await db.find_one({"_id": 1})
+    if not Find:
+        return []  # Return an empty list if no approved users are found
+    else:
+        value = Find.get("USERS", [])  # Ensure default value is an empty list
+        return value
+
+async def ADD_NEW_USER(user_id):
+    await db.update_one({"_id": 1}, {"$addToSet": {"USERS": user_id}}, upsert=True)
+
+async def REMOVE_USER(user_id):
+    await db.update_one({"_id": 1}, {"$pull": {"USERS": user_id}})
+
+async def ADD_COINS_TO_USER(user_id, coins):
+    available_users = await GET_AVAILABLE_USERS(user_id)
+    if user_id not in available_users:
+        await ADD_NEW_USER(user_id)
+    doc = {"_id": 5, f"{user_id}": coins}
     try:
         await db.insert_one(doc)
     except Exception:
         await db.update_one({"_id": 1}, {"$set": {f"{user_id}": coins}})
         
 async def GET_USER_COINS(user_id):
-    Find = await db.find_one({"_id": 1})
+    Find = await db.find_one({"_id": 5})
     if not Find:
         return None
     else:
