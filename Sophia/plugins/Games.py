e103a157ce1a5482f955e6422d6764ca81f5720a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 22:57:51 +0530

    Update Games.py

--- Sophia/plugins/Games.py
+++ Sophia/plugins/Games.py
@@ -45,3 +45,30 @@ async def send_coins(_, message):
             return await message.reply(f"Error: {send_status}")
     else:
         return await message.reply("You need to reply to a user to send coins.")
+
+@Sophia.on_message(filters.command("profile", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def Get_or_create_profile(_, message):
+    users_list = await GET_AVAILABLE_USERS()
+    if message.from_user.id in users_list:
+        await message.reply("available true:,")
+    else:
+        await message.reply("""
+**Welcome To Hyper Games ©**
+
+- You need Create a account to use Hyper Games bots.
+- We Never Use Your Data's
+- We Never Ask Personal Details In Account Creating Process
+- If you send `/continue` Your account will be created.""")
+        @Sophia.on_message(filters.command("continue", prefixes=HANDLER) & filters.user(OWNER_ID))
+        async def Create_account(_, message):
+            TEXT = await message.reply("Account Creating...")
+            await ADD_NEW_USER(message.from_user.id)
+            await ADD_COINS_TO_USER(message.from_user.id, 1000)
+            user_list_up = await GET_AVAILABLE_USERS()
+            if message.from_user.id not in user_list_up:
+                await TEXT.edit("Success, Now you can use all commands normally and you got bonus reward 1000coins")
+                return
+            else:
+                await message.reply("UNDER DEVELOPMENT MAYBE YOU RAN INTO A ERROR WE WILL FIX SOON PLEASE REPORT ON @FUTURECITY005")
+                return
+    
