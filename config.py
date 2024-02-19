8307e384fbf3f821948bc5dc823c2a4689ac127d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 14:10:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,5 +16,4 @@ MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
 IGNORED_USERS_ID = [5690711835, 1666544436, 5696053228, 2043144248] # Add the person id who you want Ignore.
 SUDO_USERS_ID = [5015417782, 6568136732] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
-Always_Approved_Users_From_Pmblock = [] # Add Users Id Who Never Want to get Warning message in pmblock.
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False # I will add soon this.
