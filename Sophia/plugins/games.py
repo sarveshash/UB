c71a92531b6a62807fe63228da180c2123ce235e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 23:48:56 +0530

    Update games.py

--- Sophia/plugins/games.py
+++ Sophia/plugins/games.py
@@ -10,13 +10,13 @@ PROFILE_TEXT = """
 **🧑‍💻 Welcome To Hyper Games ©**
 
 **• ACCOUNT REGISTRATION 🧑‍💻**
-- You need Create a account to use Hyper Games bots.
+- You need account to use Hyper Games bots.
 - We Never Use Your Data's
-- We Never Ask Any Details In Account Creating Process.
+- We Never Ask Any Details In Account Creating.
 - Your Data stored in Mongodb so may Your Data will be Disappeared sometimes.
 - If you send /continue Your account will be created.
 
-**• By sending `/continue` You Accept Our Terms and Condiitions.**
+**• By sending `/continue` You Agree Our Terms and Condiitions.**
 """
 
 USER_ID = 0
