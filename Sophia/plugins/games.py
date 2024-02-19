13486a2bb228103aa9e148460cc8481034bab3b9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 23:28:45 +0530

    Update games.py

--- Sophia/plugins/games.py
+++ Sophia/plugins/games.py
@@ -6,6 +6,28 @@ import asyncio
 import os
 from Sophia.Database.Games import *
 
-@Sophia.on_message(filters.command("send", prefixes=HANDLER) & filters.user(OWNER_ID))
-async def send_coins(_, message):
-    
+NEW_PROFILE_TEXT = """
+**Welcome To Hyper Games™ Account Registration.**
+
+- You need Create a account to use Hyper Games bots.
+- We Never Use Your Data's
+- We Never Ask Any Details In Account Creating Process.
+- Your Data stored in Mongodb so may Your Data will be Disappeared sometimes.
+- If you send /continue Your account will be created.
+**• By sending `/continue` You Accept Our Terms and Condiitions.**
+"""
+
+@Sophia.on_message(filters.command("profile", prefixes=HANDLER))
+async def get_profile(_, message):
+    LIST_USERS = await GET_AVAILABLE_USERS()
+    USER_ID = message.from_user.id
+    if USER_ID in LIST_USERS:
+        await message.reply("WAIT")
+    else:
+        await message.reply(NEW_PROFILE_TEXT)
+        @Sophia.on_message(filters.command("continue", prefixes=HANDLER))
+        async def create_profile(_, message):
+            if message.from_user.id == USER_ID:
+                await ADD_NEW_USER(USER_ID)
+                await message.reply("Thanks for creating account in Hyper Games, In this process you got 1000 coins as reward. Enjoy Have Fun")
+                return
