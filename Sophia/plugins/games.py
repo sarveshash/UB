02962eb870fc9a7c092487fde659db5818dbd69f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-19 23:42:24 +0530

    Update games.py

--- Sophia/plugins/games.py
+++ Sophia/plugins/games.py
@@ -6,7 +6,7 @@ import asyncio
 import os
 from Sophia.Database.games import *
 
-NEW_PROFILE_TEXT = """
+PROFILE_TEXT = """
 **🧑‍💻 Welcome To Hyper Games ©**
 
 **• ACCOUNT REGISTRATION 🧑‍💻**
@@ -19,16 +19,19 @@ NEW_PROFILE_TEXT = """
 **• By sending `/continue` You Accept Our Terms and Condiitions.**
 """
 
-@Sophia.on_message(filters.command("profile", prefixes=HANDLER))
+USER_ID = 0
+@Sophia.on_message(filters.command("profile", prefixes=HANDLER) & filters.group)
 async def get_profile(_, message):
+    global USER_ID
     LIST_USERS = await GET_AVAILABLE_USERS()
     USER_ID = message.from_user.id
     if USER_ID in LIST_USERS:
         await message.reply("WAIT")
     else:
-        await message.reply(NEW_PROFILE_TEXT)
-        @Sophia.on_message(filters.command("continue", prefixes=HANDLER))
+        await message.reply(PROFILE_TEXT)
+        @Sophia.on_message(filters.command("continue", prefixes=HANDLER) & filters.group)
         async def create_profile(_, message):
+            global USER_ID
             if message.from_user.id == USER_ID:
                 await ADD_NEW_USER(USER_ID)
                 await message.reply("Thanks for creating account in Hyper Games, In this process you got 1000 coins as reward. Enjoy Have Fun")
