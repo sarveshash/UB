8313b08f607a904bec7823c4a3ca63aa16fee186
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-20 14:31:59 +0530

    Update games.py

--- Sophia/Database/games.py
+++ Sophia/Database/games.py
@@ -79,10 +79,8 @@ async def BET_COINS(user_id: int, coins: int):
             LUCK_LIST = ['YES', 'NO']
             PER_50 = (coins / 100) * 50
             PER_50 = int(PER_50)
-            PER_50 = PER_50+10
             RANDOM_COINS = random.randint(PER_50, coins)
-            if RANDOM_COINS < coins:
-                RANDOM_COINS = coins
+            RANDOM_COINS = RANDOM_COINS+10
             GET_LUCK = random.choice(LUCK_LIST)
             if GET_LUCK == 'YES':
                 await ADD_COINS(user_id, RANDOM_COINS)
