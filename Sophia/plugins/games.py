5118ef1302a2d79545cbf10c3316e1fbfcc6854b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-20 10:28:25 +0530

    Update games.py

--- Sophia/plugins/games.py
+++ Sophia/plugins/games.py
@@ -26,7 +26,8 @@ async def get_profile(_, message):
     LIST_USERS = await GET_AVAILABLE_USERS()
     USER_ID = message.from_user.id
     if USER_ID in LIST_USERS:
-        await message.reply("WAIT")
+        USER_COINS = await GET_COINS_FROM_USER(USER_ID)
+        await message.reply(f"You have {USER_COINS} coins")
     else:
         await message.reply(PROFILE_TEXT)
         @Sophia.on_message(filters.command("continue", prefixes=HANDLER) & filters.group)
@@ -34,6 +35,7 @@ async def get_profile(_, message):
             global USER_ID
             if message.from_user.id == USER_ID:
                 await ADD_NEW_USER(USER_ID)
+                await ADD_COINS(USER_ID, 1000)
                 await message.reply("Thanks for creating account in Hyper Games, In this process you got 1000 coins as reward. Enjoy Have Fun")
                 return
 
