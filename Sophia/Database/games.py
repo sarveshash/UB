1fd85e6391dcf1d1a76af63a4b16f20f0f3c098e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-20 13:12:26 +0530

    Update games.py

--- Sophia/Database/games.py
+++ Sophia/Database/games.py
@@ -1,7 +1,7 @@
 from Sophia import GAME_DATABASE
 import asyncio
 
-db = GAME_DATABASE["Games_501"]
+db = GAME_DATABASE["Games_551"]
 
 async def GET_AVAILABLE_USERS():
     Find = await db.find_one({"_id": 1})
