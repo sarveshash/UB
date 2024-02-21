90c900b65647ccb803939f2c9ee16e835d16bdae
Kora <115209660+KoraXD@users.noreply.github.com>
2024-02-21 20:18:43 +0530

    .

--- Sophia/plugins/games.py
+++ Sophia/plugins/games.py
@@ -9,7 +9,7 @@ from pyrogram import enums
 import random
 from Restart import restart_program
 
-async def CHOICE_GEN(user_one: int, user_one_lvl: int, user_two: int, user_two_lvl: int):
+async def choice_generator(user_one: int, user_one_lvl: int, user_two: int, user_two_lvl: int):
     if user_one_lvl > user_two_lvl:
         if user_one_lvl < 3:
             chance = 0.5
@@ -29,8 +29,8 @@ async def CHOICE_GEN(user_one: int, user_one_lvl: int, user_two: int, user_two_l
             chance = 0.5
         else:
             chance = 0.65
-        RAN_CHOICE = random.random()
-        return user_two if RAN_CHOICE < chance else user_one
+        random_choice = random.random()
+        return user_two if random_choice < chance else user_one
     else:
         user_one_lvl = user_two_lvl 
         user_two_lvl = user_one_lvl
@@ -52,48 +52,44 @@ async def CHOICE_GEN(user_one: int, user_one_lvl: int, user_two: int, user_two_l
             chance = 0.5
         else:
             chance = 0.90
-        RAN_CHOICE = random.random()
-        return user_one if RAN_CHOICE < chance else user_two
+        random_choice = random.random()
+        return user_one if random_choice < chance else user_two
 
-REG_TEXT = """
-**🧑‍💻 Welcome To Hyper Games ©**
+registration_text = """
+**🎮 Welcome To Hyper Games ©**
 
-**• ACCOUNT REGISTRATION 🧑‍💻**
-- You need account to use Hyper Games bots.
-- We Never Use Your Data's
-- We Never Ask Any Details In Account Creating.
-- Your Data stored in Mongodb so may Your Data will be Disappeared sometimes.
-- If you send /continue Your account will be created.
+**• ACCOUNT REGISTRATION 🎮**
+- Join the Hyper Games community now!
+- No personal data required.
+- Your journey begins with a simple command: /continue.
 
-**• By sending `/continue` You Agree Our Terms and Condiitions.**
+**• By sending `/continue`, you agree to our Terms and Conditions.**
 """
-
-
 
 @Sophia.on_message(filters.command("profile", prefixes=HANDLER))
 async def get_profile(_, message):
-    LIST_USERS = await GET_AVAILABLE_USERS()
-    USER_ID = message.from_user.id
+    list_users = await GET_AVAILABLE_USERS()
+    user_id = message.from_user.id
     if message.reply_to_message:
-        if message.reply_to_message.from_user.id in LIST_USERS:
-            USER_ID = message.reply_to_message.from_user.id
+        if message.reply_to_message.from_user.id in list_users:
+            user_id = message.reply_to_message.from_user.id
         else:
-            return await message.reply("The replied user don't have a account, Hyper games account is required for using this command.")
-    if USER_ID in LIST_USERS:
-        USER_COINS = await GET_COINS_FROM_USER(USER_ID)
-        PFP = await GET_PROFILE_PIC(USER_ID)
-        EXP = await GET_EXP(USER_ID)
-        NAME = await GET_USER_NAME(USER_ID)
-        LEVEL = await GET_LEVEL(USER_ID)
-        if PFP == None:
+            return await message.reply("Sorry, the user you replied to doesn't have an account. You need a Hyper Games account to use this command.")
+    if user_id in list_users:
+        user_coins = await GET_COINS_FROM_USER(user_id)
+        pfp = await GET_PROFILE_PIC(user_id)
+        exp = await GET_EXP(user_id)
+        name = await GET_USER_NAME(user_id)
+        level = await GET_LEVEL(user_id)
+        if pfp == None:
             await message.reply_photo("https://telegra.ph/file/a359e56250bd60eb192ff.jpg", caption=f"""
 **• GAMER INFO**
 
-**- Name:** {NAME}
-**- ID:** `{USER_ID}`
-**- Coins:** `{USER_COINS}`
-**- Level:** `{LEVEL}`
-**- Experience:** `{EXP}`
+**- Name:** {name}
+**- ID:** `{user_id}`
+**- Coins:** `{user_coins}`
+**- Level:** `{level}`
+**- Experience:** `{exp}`
 **- Weapons:** `None`
 **- Relationship points:** `None`
 **- Characters:** `None`
@@ -101,14 +97,14 @@ async def get_profile(_, message):
 **•> Powered by @Hyper_Speed0™**
 """)
         else:
-            await message.reply_photo(PFP, caption=f"""
+            await message.reply_photo(pfp, caption=f"""
 **• GAMER INFO**
 
-**- Name:** {NAME}
-**- ID:** {USER_ID}
-**- Coins:** `{USER_COINS}`
-**- Level:** `{LEVEL}`
-**- Experience:** `{EXP}`
+**- Name:** {name}
+**- ID:** {user_id}
+**- Coins:** `{user_coins}`
+**- Level:** `{level}`
+**- Experience:** `{exp}`
 **- Weapons:** `None`
 **- Relationship points:** `None`
 **- Characters:** `None`
@@ -117,135 +113,133 @@ async def get_profile(_, message):
 """)
     else:
         if message.chat.type == enums.ChatType.PRIVATE:
-            return await message.reply("Creating account only works on group.")
-        await message.reply(REG_TEXT)
+            return await message.reply("Account creation is only available in groups.")
+        await message.reply(registration_text)
         @Sophia.on_message(filters.command("continue", prefixes=HANDLER) & filters.group)
         async def create_profile(_, message):
-            if message.from_user.id == USER_ID:
-                await ADD_NEW_USER(USER_ID)
-                await ADD_COINS(USER_ID, 1000)
-                await message.reply("Thanks for creating account in Hyper Games, In this process you got 1000 coins as reward. Enjoy Have Fun")
+            if message.from_user.id == user_id:
+                await ADD_NEW_USER(user_id)
+                await ADD_COINS(user_id, 1000)
+                await message.reply("Welcome to Hyper Games! You've just received 1000 coins to kickstart your adventure. Enjoy!")
                 await restart_program()
                 return
 
 @Sophia.on_message(filters.command(["send", "sent", "transfer"], prefixes=HANDLER))
 async def send_coins(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Please enter the coins to send.")
+        return await message.reply_text("Please specify the amount of coins to send.")
     if not message.reply_to_message:
-        return await message.reply("You need reply a user to send coins")
+        return await message.reply("You need to reply to a user to send coins.")
     if message.reply_to_message.from_user.id == message.from_user.id:
         return await message.reply("You can't send coins to yourself.")
     coins = " ".join(message.command[1:])
     int_coins = int(coins)
-    REPLY_USR = message.reply_to_message.from_user.id
-    USER_ID = message.from_user.id
-    SEND_STATUS = await SEND_COINS(USER_ID, REPLY_USR, int_coins)
-    if SEND_STATUS == "FROM_USER_NOT_FOUND":
-        return await message.reply("You need a account to use this command")
-    elif SEND_STATUS == "TO_USER_NOT_FOUND":
-        return await message.reply("Replied user don't have a account, account is required for using this command")
-    elif SEND_STATUS == "NOT_ENOUGH_COINS":
-        return await message.reply("You don't have enough Coins to send.")
-    elif SEND_STATUS == "NOT_POSTIVE_NUMBER":
-        return await message.reply("You need enter postive integer.")
-    elif SEND_STATUS == "SUCCESS":
-        return await message.reply(f"Successfully sent `{int_coins}`.")
-    elif SEND_STATUS.startswith("ERROR"):
-        return await message.reply(SEND_STATUS)
+    reply_usr = message.reply_to_message.from_user.id
+    user_id = message.from_user.id
+    send_status = await SEND_COINS(user_id, reply_usr, int_coins)
+    if send_status == "FROM_USER_NOT_FOUND":
+        return await message.reply("You need an account to use this command.")
+    elif send_status == "TO_USER_NOT_FOUND":
+        return await message.reply("The user you replied to doesn't have an account. Accounts are required for this command.")
+    elif send_status == "NOT_ENOUGH_COINS":
+        return await message.reply("You don't have enough coins to send.")
+    elif send_status == "NOT_POSITIVE_NUMBER":
+        return await message.reply("Please enter a positive integer.")
+    elif send_status == "SUCCESS":
+        return await message.reply(f"Successfully sent `{int_coins}` coins.")
+    elif send_status.startswith("ERROR"):
+        return await message.reply(send_status)
 
 @Sophia.on_message(filters.command("bet", prefixes=HANDLER))
 async def bet_coins(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Please enter the coins to bet.")
+        return await message.reply_text("Please enter the amount of coins to bet.")
     coins = " ".join(message.command[1:])
     int_coins = int(coins)
-    USER_ID = message.from_user.id
-    BET_STATUS = await BET_COINS(USER_ID, int_coins)
-    if BET_STATUS == "USER_NOT_FOUND":
-        return await message.reply("You need a account to use this command")
-    elif BET_STATUS == "NOT_ENOUGH_COINS":
+    user_id = message.from_user.id
+    bet_status = await BET_COINS(user_id, int_coins)
+    if bet_status == "USER_NOT_FOUND":
+        return await message.reply("You need an account to use this command.")
+    elif bet_status == "NOT_ENOUGH_COINS":
         return await message.reply("You don't have enough coins to bet.")
-    elif BET_STATUS == "NOT_POSTIVE_NUMBER":
-        return await message.reply("You need enter postive integer.")
-    elif BET_STATUS.startswith("ERROR"):
-        return await message.reply(BET_STATUS)
-    elif BET_STATUS == "LOSE":
-        return await message.reply(f"You lose {int_coins}.")
-    elif BET_STATUS == "PRO":
-        return await message.reply(f"Pro bet, you won {int_coins*2}")
+    elif bet_status == "NOT_POSITIVE_NUMBER":
+        return await message.reply("Please enter a positive integer.")
+    elif bet_status.startswith("ERROR"):
+        return await message.reply(bet_status)
+    elif bet_status == "LOSE":
+        return await message.reply(f"Sorry, you lost {int_coins} coins.")
+    elif bet_status == "PRO":
+        return await message.reply(f"Congratulations! You won {int_coins*2} coins with a pro bet!")
     else:
-        return await message.reply(f"You won {BET_STATUS} coins")
-    
+        return await message.reply(f"You won {bet_status} coins!")
+
 @Sophia.on_message(filters.command(["setpfp", "setprofile"], prefixes=HANDLER))
-async def set_pfp(_, message):
+async def set_profile_photo(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Please enter your image link to set.")
-    IMAGE_LINK = " ".join(message.command[1:])
-    if IMAGE_LINK.startswith("https://"):
-        STATUS = await SET_PROFILE_PIC(message.from_user.id, IMAGE_LINK)
-        if STATUS == "USER_NOT_FOUND":
-            return await message.reply("You need a account to use this command.")
-        elif STATUS == "NOT_ENOUGH_COINS":
-            return await message.reply("You don't have enough coins to use this command, you need atleast 1000 coins to use this")
-        elif STATUS == "SUCCESS":
-            return await message.reply("Success!, I have updated your profile picture")
+        return await message.reply_text("Please provide a valid image link to set as your profile picture.")
+    image_link = " ".join(message.command[1:])
+    if image_link.startswith("https://"):
+        status = await SET_PROFILE_PIC(message.from_user.id, image_link)
+        if status == "USER_NOT_FOUND":
+            return await message.reply("You need an account to use this command.")
+        elif status == "NOT_ENOUGH_COINS":
+            return await message.reply("You need at least 1000 coins to use this command.")
+        elif status == "SUCCESS":
+            return await message.reply("Success! Your profile picture has been updated.")
     else:
-        return await message.reply("Please enter a valid image link to set.")
+        return await message.reply("Please provide a valid image link starting with 'https://'.")
 
 @Sophia.on_message(filters.command(["setname", "setnewname"], prefixes=HANDLER))
 async def set_name(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("Please enter your new name to set.")
-    NAME_NEW = " ".join(message.command[1:])
-    STATUS = await SET_USER_NAME(message.from_user.id, NAME_NEW)
-    if STATUS == "USER_NOT_FOUND":
-        return await message.reply("You need a account to use this command")
-    elif STATUS == "NOT_ENOUGH_COINS":
-        return await message.reply("You don't have enough coins to use this command, you need atleast 1999 coins to use this")
-    elif STATUS == "SUCCESS":
-        return await message.reply("Success!, I have updated your profile name")
-        
-
+        return await message.reply_text("Please enter your new name.")
+    new_name = " ".join(message.command[1:])
+    status = await SET_USER_NAME(message.from_user.id, new_name)
+    if status == "USER_NOT_FOUND":
+        return await message.reply("You need an account to use this command.")
+    elif status == "NOT_ENOUGH_COINS":
+        return await message.reply("You need at least 1999 coins to use this command.")
+    elif status == "SUCCESS":
+        return await message.reply("Success! Your profile name has been updated.")
 
 @Sophia.on_message(filters.command("fight", prefixes=HANDLER))
 async def fight(_, message):
     if message.reply_to_message:
         if message.reply_to_message.from_user.id == message.from_user.id:
-            return await message.reply("How you can fight to yourself?")
-        LIST_USERS = await GET_AVAILABLE_USERS()
-        REPLY_USER = message.reply_to_message.from_user.id
-        USER_ID = message.from_user.id
-        COINS_RPL_USR = await GET_COINS_FROM_USER(REPLY_USER)
-        COINS_FR_USR = await GET_COINS_FROM_USER(USER_ID)
-        if USER_ID not in LIST_USERS:
-            return await message.reply("You need a account to use this command")
-        elif REPLY_USER not in LIST_USERS:
-            return await message.reply("Replied user don't have account, account is required to use this command.")
-        elif COINS_RPL_USR < 500:
-            return await message.reply("Replied user need atleast 500 coins to fight")
-        elif COINS_FR_USR < 500:
-            return await message.reply("You Need atleast 500 coins to fight")
-        FIGHT = await message.reply("Fight started")
+            return await message.reply("You can't fight yourself!")
+        list_users = await GET_AVAILABLE_USERS()
+        reply_user = message.reply_to_message.from_user.id
+        user_id = message.from_user.id
+        coins_reply_user = await GET_COINS_FROM_USER(reply_user)
+        coins_user = await GET_COINS_FROM_USER(user_id)
+        if user_id not in list_users:
+            return await message.reply("You need an account to use this command.")
+        elif reply_user not in list_users:
+            return await message.reply("The user you replied to doesn't have an account. Accounts are required for this command.")
+        elif coins_reply_user < 500:
+            return await message.reply("The user you replied to needs at least 500 coins to fight.")
+        elif coins_user < 500:
+            return await message.reply("You need at least 500 coins to fight.")
+        fight_message = await message.reply("The fight has begun!")
         await asyncio.sleep(1)
-        await FIGHT.edit("•")
+        await fight_message.edit("•")
         await asyncio.sleep(1)
-        await FIGHT.edit("••")
+        await fight_message.edit("••")
         await asyncio.sleep(0.6)
-        await FIGHT.edit("•••")
+        await fight_message.edit("•••")
         await asyncio.sleep(0.6)
-        FR_FIRST_NAME = message.from_user.first_name
-        RP_FIRST_NAME = message.reply_to_message.from_user.first_name
-        FR_USR_LVL = await GET_LEVEL(USER_ID)
-        RPL_USR_LVL = await GET_LEVEL(REPLY_USER)
-        RAN_CHOICE = await CHOICE_GEN(USER_ID, FR_USR_LVL, REPLY_USER, RPL_USR_LVL)
-        if RAN_CHOICE == USER_ID:
-            SEND = await SEND_COINS(REPLY_USER, USER_ID, 500)
-            if SEND == "SUCCESS":
-                return await FIGHT.edit(f"Fight was so interesting but winer is {FR_FIRST_NAME} got 500 coins from {RP_FIRST_NAME}")
+        fr_first_name = message.from_user.first_name
+        rp_first_name = message.reply_to_message.from_user.first_name
+        fr_user_lvl = await GET_LEVEL(user_id)
+        rp_user_lvl = await GET_LEVEL(reply_user)
+        random_choice = await choice_generator(user_id, fr_user_lvl, reply_user, rp_user_lvl)
+        if random_choice == user_id:
+            send_coins_status = await SEND_COINS(reply_user, user_id, 500)
+            if send_coins_status == "SUCCESS":
+                return await fight_message.edit(f"The fight was intense, but the winner is {fr_first_name}! They received 500 coins from {rp_first_name}.")
         else:
-            SEND = await SEND_COINS(USER_ID, REPLY_USER, 500)
-            if SEND == "SUCCESS":
-                return await FIGHT.edit(f"Fight was so interesting but winer is {RP_FIRST_NAME} got 500 coins from {FR_FIRST_NAME}")
+            send_coins_status = await SEND_COINS(user_id, reply_user, 500)
+            if send_coins_status == "SUCCESS":
+                return await fight_message.edit(f"The fight was intense, but the winner is {rp_first_name}! They received 500 coins from {fr_first_name}.")
     else:
-        return await message.reply("Please reply a user to fight.")
+        return await message.reply("Please reply to a user to initiate a fight.")
