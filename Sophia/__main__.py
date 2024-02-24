a0ad7d81c81d7af5564fd26c8e0efc972f46c7ac
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-24 19:37:58 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -18,8 +18,7 @@ PWD = f"{os.getcwd()}/"
 if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
-        Sophia.start()
-        pyrogram.idle()
+        Sophia.run()
     else:
         raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
         exit()
