ca6c5ba220667481c4feef1ffcc98b02a556525c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-24 23:22:57 +0530

    Update Broadcast.py

--- Sophia/plugins/Broadcast.py
+++ Sophia/plugins/Broadcast.py
@@ -4,10 +4,30 @@ from config import OWNER_ID
 from pyrogram import filters
 import asyncio
 import os
-from Sophia.Database.Broadcast import *
+from pyrogram import enums
 
 @Sophia.on_message(filters.command("broadcastall", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def broadcast_all(_, message):
-    LIST_CHATS = await GET_ALL_CHATS()
-    for CHAT_ID in LIST_CHATS:
-        await Sophia.send_message(CHAT_ID, "#TESTING BROADCAST\n - Powered by @Hyper_Speed0")
+    SUCCESS = 0
+    FAILED = 0
+    if message.reply_to_message:
+        async for dialog in Sophia.get_dialogs():
+            if not dialog.chat.type == enums.ChatType.CHANNEL or not dialog.chat.type == enums.ChatType.BOT:
+                try:
+                    await Sophia.forward_messages(dialog.chat.id, message.chat.id, message.reply_to_message_id)
+                    SUCCESS += 1
+                except Exception:
+                    FAILED += 1
+        await message.reply(f"**Broadcast Complete**\n\nSUCCESS = {SUCCESS}\nFAILED = {FAILED}")
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text("➲ Master, Please enter a text to broadcast or reply a message to broadcast.")
+        text = message.text.split(None, 1)[1]
+        async for dialog in Sophia.get_dialogs():
+            if not dialog.chat.type == enums.ChatType.CHANNEL or not dialog.chat.type == enums.ChatType.BOT:
+                try:
+                    await Sophia.send_message(dialog.chat.id, text)
+                    SUCCESS += 1
+                except Exception:
+                    FAILED += 1
+        await message.reply(f"**Broadcast Complete**\n\nSUCCESS = {SUCCESS}\nFAILED = {FAILED}")
