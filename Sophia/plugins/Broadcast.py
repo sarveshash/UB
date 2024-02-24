bfafd2326e0e22e7b0da427b0ff0a9d25899fe5f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-24 18:48:57 +0530

    Update Broadcast.py

--- Sophia/plugins/Broadcast.py
+++ Sophia/plugins/Broadcast.py
@@ -8,4 +8,6 @@ from Sophia.Database.Broadcast import *
 
 @Sophia.on_message(filters.command("broadcastall", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def broadcast_all(_, message):
-    # Ntg
+    LIST_CHATS = await GET_ALL_CHATS()
+    for CHAT_ID in LIST_CHATS:
+        await Sophia.send_message(CHAT_ID, "#TESTING BROADCAST\n - Powered by @Hyper_Speed0")
