a8f02585426e629589fcf95179213dfca6c970e7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-26 21:16:34 +0530

    Update backup_msg.py

--- Sophia/Database/backup_msg.py
+++ Sophia/Database/backup_msg.py
@@ -28,7 +28,7 @@ async def REMOVE_BACKUP_CHAT(chat_id: int):
     await db.update_one({"_id": 1}, {"$pull": {"CHATS": chat_id}})
 
 async def SET_BACKUP_CHANNEL_ID(user_id, channel_id):
-    await await db.update_one({"_id": 1}, {"$addToSet": {f"{user_id}": channel_id}})
+    await db.update_one({"_id": 1}, {"$addToSet": {f"{user_id}": channel_id}})
 
 async def GET_BACKUP_CHANNEL_ID(chat_id):
     Find = await db.find_one({"_id": 1})
