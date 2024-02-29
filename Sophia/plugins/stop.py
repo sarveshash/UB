6da0cc97d7f32297a388736b5eae210c913412d7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-29 16:51:56 +0530

    Update stop.py

--- Sophia/plugins/stop.py
+++ Sophia/plugins/stop.py
@@ -8,8 +8,8 @@ from Restart import restart_program as restart
 
 @Sophia.on_message(filters.command("stop", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def stop_ub(_, message):
-    await message.reply("I have stopped for 5sec")
-    Sophia.stop()
-    await asyncio.sleep(5)
+    await message.reply("I have stopped for 15sec")
+    await Sophia.stop()
+    await asyncio.sleep(15)
     await restart()
     
