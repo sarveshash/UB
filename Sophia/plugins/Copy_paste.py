1f1ba8c3d933c25fe42eced16220c2bb45977e42
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-02 21:56:25 +0530

    Update Copy_paste.py

--- Sophia/plugins/Copy_paste.py
+++ Sophia/plugins/Copy_paste.py
@@ -6,36 +6,34 @@ from Sophia.Database.copy_msg import *
 import asyncio
 import os
 
-COPIED_MSG = 0
-COPIED_MSG_CHAT = 0
-COPIED = False
-
 @Sophia.on_message(filters.command("copy", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def Copy_msg(_, message):
-    global COPIED_MSG, COPIED_MSG_CHAT, COPIED
     if not message.reply_to_message:
         return await message.reply("Reply a message to copy it.")
     else:
         try:
             COPIED_MSG = message.reply_to_message_id
             COPIED_MSG_CHAT = message.chat.id
-            COPIED = True
-            await message.reply("Successfully copied!")
+            STORE = await SAVE_MSG(COPIED_MSG, COPIED_MSG_CHAT)
+            if STORE == "SUCCESS":
+                await message.reply("Successfully copied!")
+            else:
+                await message.reply(f"Error, {STORE}")
         except Exception as e:
             return await message.reply("Error", e)
 
 @Sophia.on_message(filters.command("paste", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def paste_msg(_, message):
-    global COPIED_MSG, COPIED_MSG_CHAT, COPIED
+    COPIED = await COPIED()
     if not COPIED == True:
         return await message.reply("Clipboard is empty.")
     else:
-        await Sophia.copy_message(message.chat.id, COPIED_MSG_CHAT, COPIED_MSG)
+        await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID())
 
 @Sophia.on_message(filters.command("ncpaste", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def no_caption_paste_msg(_, message):
-    global COPIED_MSG, COPIED_MSG_CHAT, COPIED
+    COPIED = await COPIED()
     if not COPIED == True:
         return await message.reply("Clipboard is empty.")
     else:
-        await Sophia.copy_message(message.chat.id, COPIED_MSG_CHAT, COPIED_MSG, caption="")
+        await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), caption="")
