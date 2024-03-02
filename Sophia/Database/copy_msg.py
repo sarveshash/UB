292e3be18d3b30370aedbb38f8b856bac58d96d1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-02 21:55:56 +0530

    Update copy_msg.py

--- Sophia/Database/copy_msg.py
+++ Sophia/Database/copy_msg.py
@@ -5,7 +5,11 @@ import random
 db = DATABASE["Copy_message"]
 
 async def SAVE_MSG(msg_id: int, msg_chat: int):
-    await db.update_one({"_id": 1}, {"$addToSet": {"COPIED": True, "CHAT": msg_chat, "ID": msg_id}})
+    try:
+        await db.update_one({"_id": 1}, {"$addToSet": {"COPIED": True, "CHAT": msg_chat, "ID": msg_id}})
+        return "SUCCESS"
+    except Exception as e:
+        return str(e)
 
 async def COPIED():
     Find = await db.find_one({"_id": 1})
@@ -14,6 +18,16 @@ async def COPIED():
     else:
         value = Find["COPIED"]
         return value
+
+async def UNSAVE_MSG():
+    COPIED = await COPIED()
+    if not COPIED == True:
+        return "ALREADY_EMPTY"
+    try:
+        await db.update_one({"_id": 1}, {"$addToSet": {"COPIED": False, "CHAT": 0, "ID": 0}})
+        return "SUCCESS"
+    except Exception as e:
+        return str(e)
 
 async def CHAT_ID():
     Find = await db.find_one({"_id": 1})
