c929e87d358ee5cf71277a9a57e84452c94fca8b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-02 21:42:47 +0530

    Update copy_msg.py

--- Sophia/Database/copy_msg.py
+++ Sophia/Database/copy_msg.py
@@ -6,3 +6,5 @@ db = DATABASE["Copy_message"]
 
 async def SAVE_MSG(msg_id: int, msg_chat: int):
     await db.update_one({"_id": 1}, {"$addToSet": {"STATUS": True, "CHAT": msg_chat, "ID": msg_id}})
+
+""
