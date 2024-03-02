964b20e3dce714ab822c79a189d6303dc3948039
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-02 21:40:51 +0530

    Update copy_msg.py

--- Sophia/Database/copy_msg.py
+++ Sophia/Database/copy_msg.py
@@ -5,4 +5,4 @@ import random
 db = GAME_DATABASE["Games_RO"]
 
 async def SAVE_MSG(msg_id: int, msg_chat: int):
-    await db.update_one({"_id": 1}, {"$addToSet": {"STATUS": True}, {"CHAT": msg_chat}, {"ID": msg_id}}
+    await db.update_one({"_id": 1}, {"$addToSet": {"STATUS": True, "CHAT": msg_chat, "ID": msg_id}})
