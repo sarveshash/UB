a4081ee121d6167ac25cdfc3214198a45c623845
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-04 08:19:53 +0530

    Update telegraph.py

--- Sophia/plugins/telegraph.py
+++ Sophia/plugins/telegraph.py
@@ -6,21 +6,41 @@ from Sophia.__main__ import Sophia
 from Sophia import HANDLER
 
 @Sophia.on_message(filters.command(["tgm", "tm"], prefixes=HANDLER) & filters.user(OWNER_ID))
-async def gen_telegraph(_, message):
-    reply = message.reply_to_message
-    if not reply:
-        return await message.reply("Master, reply to a media!")
-    if reply.media:
-      try:
-          i = await message.reply("`Generating Telegraph Link...`")
-          path = await reply.download()
-          fk = upload_file(path)
-          for x in fk:
-              url = "https://graph.org/" + x
-          await i.edit(f'**Your Telegraph Link Is Generated** [🎉](`{url}`)', disable_web_page_preview=True)
-          os.remove(path)
-      except Exception as e:
-          await i.edit(f"ERROR: **{e}**")
-          
-          
-# This code all rights reserved to © "https://t.me/Kalakar_Sangram"
+async def telegraph(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply("Reply to Video or Image")
+        return
+    if not (
+        (replied.photo and replied.photo.file_size <= 5242880)
+        or (replied.animation and replied.animation.file_size <= 5242880)
+        or (
+            replied.video
+            and replied.video.file_name.endswith(".mp4")
+            and replied.video.file_size <= 5242880
+        )
+        or (
+            replied.document
+            and replied.document.file_name.endswith(
+                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+            )
+            and replied.document.file_size <= 5242880
+        )
+    ):
+        await message.reply("Oops! File Not Supported bruh")
+        return
+    location1 = await client.download_media(
+        message=message.reply_to_message,
+        file_name="Sophia/downloads/",
+    )
+    try:
+        response = upload_file(location1)
+    except Exception as document:
+        await message.reply(message, text=document)
+    else:
+        await message.reply(
+            f"**Your link Generated** 👉 `https://telegra.ph{response[0]}` ",
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(location1)
