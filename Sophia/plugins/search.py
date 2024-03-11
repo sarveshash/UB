386109cd46556e73357691475fc92b96426ecd4d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-11 09:23:24 +0530

    Update search.py

--- Sophia/plugins/search.py
+++ Sophia/plugins/search.py
@@ -8,10 +8,11 @@ from googlesearch import search
 
 @Sophia.on_message(filters.command("search", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def search(_, message):
-    if len(message.text.split()) <2:
+    if len(message.text.split()) < 2:
         return await message.reply("Master, enter a text to search it.")
+    MSG = await message.reply("`Loading...`")
     query = " ".join(message.command[1:])
     links = ""
-    for j in search(query, num=10, stop=10, pause=2):
+    for j in search(query, stop=10, pause=2):
         links += f"{j}\n"
-    await message.reply(f"**Results:**:\n{h}", disable_web_page_preview=True)
+    await MSG.edit(f"**Results:**:\n\n{links}", disable_web_page_preview=True)
