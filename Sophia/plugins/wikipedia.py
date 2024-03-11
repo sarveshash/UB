25621afc525bc4f3363627a35fa5341fa5dffce6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-11 10:27:16 +0530

    Update wikipedia.py

--- Sophia/plugins/wikipedia.py
+++ Sophia/plugins/wikipedia.py
@@ -4,6 +4,7 @@ from config import OWNER_ID
 from pyrogram import filters
 import asyncio
 import os
+import io
 import wikipedia
 
 @Sophia.on_message(filters.command(["wiki", "wikipedia"], prefixes=HANDLER) & filters.user(OWNER_ID))
@@ -19,7 +20,15 @@ async def search_wikipedia(_, message):
             return await MSG.edit("No results found.")
         # Get summary for the first result
         summary = wikipedia.summary(results[0])
-        await MSG.edit(f"**Results from Wikipedia for '{query}':**\n\n{summary}")
+        if len(summary) > 3700:
+            summary = f"**Results from Wikipedia for '{query}':\n\n{summary}"
+            with io.BytesIO(str.encode(summary)) as output:
+                output.name = "wikipedia.txt"
+                await message.reply_document(
+                    document=output
+                )
+        else:
+            await MSG.edit(f"**Results from Wikipedia for '{query}':**\n\n{summary}")
     except wikipedia.exceptions.DisambiguationError as e:
         await MSG.edit(f"DisambiguationError: {e}")
     except wikipedia.exceptions.PageError as e:
