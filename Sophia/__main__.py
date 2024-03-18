80634c343758320d07358f598967d1c656ee4cbb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-18 06:48:05 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
         try:
-            Sgophia.rugn()
+            pund
         except Exception as e:
-            ErrorPrinter.run()
+            ErrorPrinter.start()
             with ErrorPrinter:
                 ErrorPrinter.send_message("me", f"Error starting Sophia:\n\n{e}")
                 raise Exception(e)
