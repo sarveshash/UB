e67c59888d324e3d5e81c1fed31e33d4b44f8827
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-18 07:07:53 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -1,8 +1,10 @@
 from Sophia import *
 from pyrogram import Client, filters
 import os
+import io
 import logging
 import pyrogram
+from subprocess import getoutput as run
 from Restart import restart_program
 
 
@@ -12,8 +14,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-logging.basicConfig(filename='error.txt', level=logging.ERROR)
-
 PWD = f"{os.getcwd()}/"
 
 
@@ -21,13 +21,25 @@ if __name__ == "__main__":
     ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
     if ACCESS == "oTaZUki004nandhaiSgeY":
         try:
-            Sophia.run()
+            otazuki
         except Exception as e:
             ErrorPrinter.run()
             with ErrorPrinter:
-                ErrorPrinter.send_message("me", f"Error starting Sophia:\n\n{e}")
-                raise Exception(e)
+                run_logs = run("cat log.txt")
+                if len(run_logs) >= 3000:
+                    with io.BytesIO(str.encode(run_logs)) as logs:
+                        logs.name = "ERROR.txt"
+                        ErrorPrinter.send_document(
+                            "me",
+                            document=logs,
+                            caption="ERROR STARTING SOPHIA",
+                        )
+                    raise Exception(run_logs)
+                else:
+                    ErrorPrinter.send_message("me", f"ERROR STARTING SOPHIA:\n\n{run_logs}")
+                    raise Exception(run_logs)
                 ErrorPrinter.stop()
+                    
     else:
         raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
         exit()
