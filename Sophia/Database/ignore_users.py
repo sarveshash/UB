0890a3061b09d047b9d0476d2c5bfb10d126f8f0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-20 18:01:36 +0530

    Update ignore_users.py

--- Sophia/Database/ignore_users.py
+++ Sophia/Database/ignore_users.py
@@ -5,4 +5,23 @@ import random
 db = DATABASE["IGNORED USERS"]
 
 class IGNORED_USERS:
-  print('hi')
+    async def ADD(self, user_id):
+        try:
+            await db.update_one({"_id": 1}, {"$addToSet": {"IGNORED_USERS": user_id}}, upsert=True)
+            return "SUCCESS"
+        except Exception as e:
+            print("Error while adding user in ignored list", e)
+            return e
+    async def GET(self):
+        Find = await db.find_one({"_id": 1})
+        if not Find:
+            return []
+        else:
+            value = Find.get("IGNORED_USERS", [])
+            return value
+    async def REMOVE(self, user_id):
+        try:
+            await db.update_one({"_id": 1}, {"$pull": {"IGNORED_USERS": user_id}})
+            return "SUCCESS"
+        except Exception as e:
+            return e
