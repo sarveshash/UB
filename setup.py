df0c395d522231f2bf7681b0443bb41c35415144
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-20 20:06:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,20 +12,20 @@ ACCESS_CODE = input("Enter ACCESS_CODE: ")
 ACCESS_PIN = input("Enter ACCESS_PIN: ")
 MONGO_DB_URI = input("Enter MONGO_DB_URI: ")
 YOUR_REPO_LINK = input("Enter your forked repo link: ")
+input("Enter to proceed!")
 
 print("[INFO] Processing values...")
 
 try:
+  os.environ['SESSION'] = SESSION
   os.environ['API_ID'] = API_ID
   os.environ['API_HASH'] = API_HASH
-  os.environ['SESSION'] = SESSION
-  os.environ['YOUR_REPO_LINK'] = YOUR_REPO_LINK
   os.environ['ACCESS_CODE'] = ACCESS_CODE
   os.environ['ACCESS_PIN'] = ACCESS_PIN
   os.environ['MONGO_DB_URI'] = MONGO_DB_URI
+  os.environ['YOUR_REPO_LINK'] = YOUR_REPO_LINK
 except Exception as e:
   raise e
-  r("python3 -m setup.py")
   exit()
 
 print("[INFO] Success, Starting Sophia..")
