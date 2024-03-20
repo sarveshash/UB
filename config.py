6a2f2fcf03f7631f403558582fa5fbe552f8a9ba
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-20 19:08:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
-"""
-⚠️ WARNING ⚠️
 
-If you don't know how to use this
-Don't add Something Wrong Here Otherwise this repo won't work, Fill Everything using Instructions from readme.md file.
-If you got problem KINDLY ASK IN SUPPORT CHAT @FUTURECITY005 OR DM ME @OTAZUKI
-I hope you will fill Everything without Any problems
-"""
+# ⚠️ WARNING ⚠️
+
+# If you don't know how to use this
+# Don't add Something Wrong Here Otherwise this repo won't work, Fill Everything using Instructions from readme.md file.
+# If you got problem, KINDLY ASK IN SUPPORT CHAT @FUTURECITY005 OR DM ME @OTAZUKI
+# I hope you will fill Everything without Any problems
 
 # REQUIRED THINGS
 OWNER_ID = 5965055071 # Replace this to your Actual Owner's I'd
-DATABASE_GROUP_ID = -1001962303988 # Add Database group id for Enchance the bot, and Sophia Runing without SQL or Mongodb So Its Required.
 
 # SETTINGS 
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
-IGNORED_USERS_ID = [] # Add the person id who you want Ignore.
 SUDO_USERS_ID = [5015417782, 6568136732] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False # I will add soon this.
