aa8b1123cc0c6c11c27cb88b5eea0723c3810e0a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-20 18:55:36 +0530

    Update Igonered_Users.py

--- Sophia/plugins/Igonered_Users.py
+++ Sophia/plugins/Igonered_Users.py
@@ -15,7 +15,7 @@ warning_count = {}
 
 @Sophia.on_message(filters.private & filters.create(ignore_users))
 async def ignored_private_chat(_, message):
-    user_id = message.from_user.id
+    user_id = message.chat.id
     if user_id not in warning_count:
         warning_count[user_id] = 0
     warning_count[user_id] += 1
@@ -35,6 +35,8 @@ async def ignored_private_chat(_, message):
 
 @Sophia.on_message(filters.command("ignore", prefixes=HANDLER) & filters.private & filters.me)
 async def add_ignored_person(_, message):
+    if message.chat.id == OWNER_ID:
+        return await message.reply("You can't ignore yourself!")
     USRS = IGNORED_USERS()
     if message.chat.id in await USRS.GET():
         return await message.reply("This person already in ignored list!")
@@ -44,7 +46,23 @@ async def add_ignored_person(_, message):
             if H == "SUCCESS":
                 await message.reply("I have started ignoring this person!")
             else:
-                await message.reply(H)
+                await message.reply(f'Error {H}')
         except Exception as e:
-            await message.reply(e)
-        
+            await message.reply(f'Error {e}')
+
+@Sophia.on_message(filters.command("unignore", prefixes=HANDLER) & filters.private & filters.me)
+async def remove_ignored_person(_, message):
+    if message.chat.id == OWNER_ID:
+        return await message.reply("You can't unignore yourself!")
+    USRS = IGNORED_USERS()
+    if message.chat.id not in await USRS.GET():
+        return await message.reply("This person is not in ignored list!")
+    else:
+        try:
+            H = await USRS.REMOVE(message.chat.id)
+            if H == "SUCCESS":
+                await message.reply("I have removed this person in ignored list!")
+            else:
+                await message.reply(f"Error, {H}")
+        except Exception as e:
+            await message.reply(f"Error {e}")
