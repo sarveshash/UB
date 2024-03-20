a3d47bd85bdaa2b5a8599022d026c584b703791b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-20 08:42:07 +0530

    Update Igonered_Users.py

--- Sophia/plugins/Igonered_Users.py
+++ Sophia/plugins/Igonered_Users.py
@@ -8,25 +8,19 @@ warning_count = {}
 @Sophia.on_message(filters.private & filters.user(IGNORED_USERS_ID))
 async def ignored_private_chat(_, message):
     user_id = message.from_user.id
-
-    # Check if the user has been warned before
     if user_id not in warning_count:
         warning_count[user_id] = 0
-
-    # Increment the warning count
     warning_count[user_id] += 1
-
-    # Warn the user
     if warning_count[user_id] == 1:
-        await message.reply("Sᴏʀʀʏ, Yᴏᴜ ᴀʀᴇ ɪɢɴᴏʀᴇᴅ ʙʏ ᴍʏ ʟᴏᴠᴇʟʏ ❤️ Mᴀsᴛᴇʀ, ɪғ ʏᴏᴜ sᴇɴᴅ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴀɢᴀɪɴ ʏᴏᴜ ᴡɪʟʟ ʙᴇ ɢᴇᴛ Bʟᴏᴄᴋᴇᴅ.")
+        await message.reply("Sorry, you are a ignored person of my master you can't chat with him!")
         await Sophia.archive_chats([message.chat.id])
     elif warning_count[user_id] == 2:
         await message.reply("This is your second warning. If you send another message, you will be blocked.")
     elif warning_count[user_id] >= 3:
         try:
-            await message.reply("Sorry, You Have Breaked Your Limits that's why I blocked You!")
+            await message.reply("Sorry, You have breaked your limits so i blocked you")
             await Sophia.block_user(user_id)
-            await Sophia.send_message(OWNER_ID, f"Master, I have Been Blocked A user From Ignored User List He/She Disturbing me i do that Hehe, If You Want Unblock Him/Her, Here is Username @{message.from_user.username}\n\n**Note:** If Want Clear all warn Do .restart or if you want remove him/her in **ignored_users** Remove This ID in your __config.py__ Here is Id: `{message.from_user.id}`")
+            await Sophia.send_message('me', f"Master, i have blocked {message.from_user.first_name}\n\nHe/she spamed in your chat so i blocked if you want unblock you can find them in archived chats! use .unignore to unignore them!")
         except Exception as e:
             print(e)
-            await Sophia.send_message(OWNER_ID, f"Sorry Master, I got an error when blocking Ignored User. Check Errors Below 💔\n {e}")
+            await Sophia.send_message('me', f"Sorry Master, I got an error when blocking Ignored User. Check Errors Below 💔\n {e}")
