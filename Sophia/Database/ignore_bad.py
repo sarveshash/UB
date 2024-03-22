97d6ab0c19e97bd99726e2f30270095b08cbc3ef
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-22 23:52:33 +0530

    Create ignore_bad.py

---
+++ Sophia/Database/ignore_bad.py
@@ -0,0 +1,28 @@
+from Sophia import DATABASE
+import asyncio
+import random
+
+db = DATABASE["IGNORE_BAD"]
+
+class IGNORE_BAD:
+    async def ENABLE(self):
+        try:
+            await db.update_one({"_id": 1}, {"$addToSet": {"IGNORE_BAD": True}})
+            return "SUCCESS"
+        except Exception as e:
+            print("Error while enabling IGNORE_BAD", e)
+            return e
+    async def GET(self):
+        Find = await db.find_one({"_id": 1})
+        if not Find:
+            return False 
+        else:
+            value = Find.get("IGNORE_BAD", False)
+            return value
+    async def REMOVE(self):
+        try:
+            await db.update_one({"_id": 1}, {"$set": {"IGNORE_BAD": False}})
+            return "SUCCESS"
+        except Exception as e:
+            return e
+
