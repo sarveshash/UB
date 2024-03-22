a595c06e98940da8a605e126be613e0eeb64a225
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-23 00:02:32 +0530

    Create ignore_bad.py

---
+++ Sophia/plugins/ignore_bad.py
@@ -0,0 +1,46 @@
+from Sophia import HANDLER
+from Sophia.__main__ import Sophia
+from config import OWNER_ID
+from Sophia.Database.ignore_bad import *
+from pyrogram import filters
+import asyncio
+import re
+
+async def bad_word_remover_stats(_, client, update):
+    ignore_bad = IGNORE_BAD()
+    if await ignore_bad.GET == True and not update.chat.id == OWNER_ID:
+        return True
+    else:
+        return False
+
+# List of words to filter
+bad_words = [
+    'punda', 'fuck', 'ommala'
+]
+
+pattern = r"\b(?:{})\b".format('|'.join(['{}(?:{})?'.format(re.escape(word), '[a-zA-Z]*' * (len(word)-1)) for word in bad_words]))
+
+@Sophia.on_message(filters.text & filters.regex(pattern, re.IGNORECASE) & filters.create(bad_word_remover_stats))
+async def remove_message(_, message):
+    try:
+        await message.delete()
+    except Exception as e:
+        if str(e) == """Telegram says: [403 MESSAGE_DELETE_FORBIDDEN] - You don't have rights to delete messages in this chat, most likely because you are not the author of them (caused by "channels.DeleteMessages")""":
+            return
+        print(e)
+            await Sophia.send_message(message.chat.id, f"Error: {e}")
+
+@Sophia.on_message(filters.command(["ignorebad", "stopbad"], prefixes=HANDLER) & filters.me)
+async def set_ignore_bad(_, message):
+    try:
+        ignore_bad = IGNORE_BAD()
+        log = await ignore_bad.ENABLE()
+        if log == "SUCCESS":
+            await message.reply("Let's ignore bad things!")
+        else:
+            await message.reply(f"Error: {e}")
+            print(e)
+    except Exception as e:
+        await message.reply(f"Error: {e}")
+        print(e)
+        
