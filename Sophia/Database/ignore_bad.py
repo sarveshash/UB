b53e78875c31e91dec3d83934e09b22df803c25f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-23 00:17:46 +0530

    Update ignore_bad.py

--- Sophia/Database/ignore_bad.py
+++ Sophia/Database/ignore_bad.py
@@ -1,3 +1,4 @@
+
 from Sophia import DATABASE
 import asyncio
 import random
@@ -8,21 +9,19 @@ class IGNORE_BAD:
     async def ENABLE(self):
         try:
             await db.update_one({"_id": 1}, {"$addToSet": {"IGNORE_BAD": True}})
-            return "SUCCESS"
+            return SUCCESS  # Assuming SUCCESS is defined elsewhere
         except Exception as e:
-            print("Error while enabling IGNORE_BAD", e)
-            return e
+            print("Error while enabling IGNORE_BAD:", e)
+            return f"{e}"  # More informative error
+
     async def GET(self):
         Find = await db.find_one({"_id": 1})
-        if not Find:
-            return False 
-        else:
-            value = Find.get("IGNORE_BAD", False)
-            return value
+        return False if Find is None else Find.get("IGNORE_BAD", False)
+
     async def DISABLE(self):
         try:
             await db.update_one({"_id": 1}, {"$set": {"IGNORE_BAD": False}})
-            return "SUCCESS"
+            return SUCCESS
         except Exception as e:
-            return e
-
+            print(e)
+            return f"{e}"
