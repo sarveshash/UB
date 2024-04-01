1aa4241250d15b01a873368c5267cf33faab4624
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-01 18:07:17 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -11,9 +11,4 @@ PWD = f"{os.getcwd()}/"
 
 
 if __name__ == "__main__":
-    ACCESS = decode_key(ACCESS_CODE, ACCESS_PIN)
-    if ACCESS == "oTaZUki004nandhaiSgeY":
-        Sophia.run()
-    else:
-        raise Exception("[INFO] Invalid Access Key, Access Key is required to Use Sophia Beta, Try Again")
-        exit()
+    Sophia.run()
