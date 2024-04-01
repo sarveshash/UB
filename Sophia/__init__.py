b0f340944033bf7198daed5092359a572c6696a5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-01 20:13:22 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -16,14 +16,76 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+# IDK
+HM = os.environ.get("SESSION")
+if HM == None:
+    OLD_DATAS_DB = input("Enter your mongodb uri: ") # For getting user values
+    DATABASE = AsyncIOMotorClient(OLD_DATAS_DB)["SOPHIA_UB"]
+    db = DATABASE["USER_DATAS"]
+    async def GET_VALUES():
+        Find = await db.find_one({"_id": 404})
+        if not Find:
+            return False
+        else:
+            return Find
+    VALUES = GET_VALUES()
+    count = 0
+    def inc_count():
+        global count
+        count += 1
+    for ai_rule_world in VALUES:
+        if count == 0:
+            r(f"export SESSION={ai_rule_world}")
+            inc_count()
+        elif count == 1:
+            r(f"export API_ID={ai_rule_world}")
+            inc_count()
+        elif count == 2:
+            r(f"export API_HASH={ai_rule_world}")
+            inc_count()
+        elif count == 3:
+            r(f"export MONGO_DB_URI={OLD_DATAS_DB}")
+            inc_count()
+        elif count == 4:
+            r(f"export YOUR_REPO_LINK={ai_rule_world}")
+            inc_count()
+            rs_pg()
+    while VALUES is not None:
+        try:
+            while True:
+                SESSION = input("Enter you Pyrogram V-2 Session: ")
+                if len(SESSION) < 50:
+                    print("[Sophia System] Enter session correctly!")
+                else:
+                    break
+            API_ID = input("Enter Api ID: ")
+            API_HASH = input("Enter Api hash: ")
+            MONGO_DB_URI = input("Enter MONGO_DB_URI: ")
+            YOUR_REPO_LINK = input("Enter your forked repo link: ")
+            input("Enter to proceed: ")
+            
+            DATABASE = AsyncIOMotorClient(OLD_DATAS_DB)["SOPHIA_UB"]
+            db = DATABASE["USER_DATAS"]
+            
+            async def SET_VALUES(session, a_id, a_hash, repo):
+                doc = {"_id": 404, "session": session, "a_id": a_id, "a_hash": a_hash, "repo": repo, "everything": True}
+                try:
+                    await db.insert_one(doc)
+                except Exception:
+                    await db.update_one({"_id": 404}, {"$set": {"session": session, "a_id": a_id, "a_hash": a_hash, "repo": repo, "everything": True}})
+            SET_VALUES(SESSION, API_ID, API_HASH, YOUR_REPO_LINK)
+            break
+        except Exception as e:
+            print(e)
+
 # VARIABLES
-SESSION = "BQCbcXYAgPaTnqIB-FAL2f82IDDVQbp9cr21B3vT70Mol6zGzx1CkmJUYhoGRYOc-UxeAU2IR9mDAdDDuaeL798asb9KmCUQDZG7rZNNuErDhnzDOePGpdvjj27t-acB1j7lEx3adRAYmln76xmy94uGkFoAcMQ-9zdrCXbmZZ-R00_Me4i5ZgVUvKFhQIPzWogeziLp-gzqo7DCBwI9YeaF8JbwMShmdhKHHwRjotEJS6C_VrSJBOx8ZkyglngEn3k27xRo6efc9SEIOYApi_dULv_2RETFmMrmJBw56VR-VhIDvAO0hoajTdhnq5YKXG2XLGBasKlp0mQ7pMpYf52YcpXYtAAAAAFji4RfAA"
-API_ID = 10187126
-API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
+SESSION = os.environ.get("SESSION")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
 HANDLER = ["~",".","!","/","$","#"]
 LOG_CHANNEL = -1002010994783
-MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
-REPO_URL = "https://github.com/Otazuki004/SophiaUB/"
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+REPO_URL = os.environ.get("YOUR_REPO_LINK")
 MY_VERSION = 0.00219
 
 # GETTING REPO NAME USED FOR UPDATE MODULE
