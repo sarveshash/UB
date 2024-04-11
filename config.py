f8939246235d30817cea9b0668367b5c5403dfd8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-11 20:33:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OWNER_ID = 5965055071 # Replace this to your Actual Owner's I'd
 
 # SETTINGS 
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
-SUDO_USERS_ID = [5015417782, 6568136732] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
+SUDO_USERS_ID = [] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False # I will add soon this.
