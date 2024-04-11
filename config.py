0781619b5051989b13c83d98277293ffb40a42ca
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-11 20:48:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
+from Sophia.__main__ import id
 
-# ⚠️ WARNING ⚠️
-
-# If you don't know how to use this
-# Don't add Something Wrong Here Otherwise this repo won't work, Fill Everything using Instructions from readme.md file.
-# If you got problem, KINDLY ASK IN SUPPORT CHAT @FUTURECITY005 OR DM ME @OTAZUKI
-# I hope you will fill Everything without Any problems
-
-# REQUIRED THINGS
-OWNER_ID = 5965055071 # Replace this to your Actual Owner's I'd
+# VARIABLES
+OWNER_ID = id
 
 # SETTINGS 
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
