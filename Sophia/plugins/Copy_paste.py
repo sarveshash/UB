e23639c34e17bd82843c51032bc0866e3488b601
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-15 17:14:11 +0530

    Update Copy_paste.py

--- Sophia/plugins/Copy_paste.py
+++ Sophia/plugins/Copy_paste.py
@@ -12,6 +12,8 @@ async def Copy_msg(_, message):
         return await message.reply("Reply to a message to copy it.")
     else:
         try:
+            if message.reply_to_message.media_group_id:
+                
             COPIED_MSG = message.reply_to_message_id
             COPIED_MSG_CHAT = message.chat.id
             STORE = await SAVE_MSG(COPIED_MSG, COPIED_MSG_CHAT)
@@ -38,12 +40,16 @@ async def paste_msg(_, message):
     else:
         try:
             if message.reply_to_message:
+                if await IS_ALBUM() == True:
+                    return await Sophia.copy_media_group(message.chat.id, await CHAT_ID(), await MSG_ID(), reply_to_message_id=message.reply_to_message_id)
                 return await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), reply_to_message_id=message.reply_to_message_id)
-            await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID())
+            if await IS_ALBUM() == True:
+                return await Sophia.copy_media_group(message.chat.id, await CHAT_ID(), await MSG_ID())
+            return await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID())
         except Exception as e:
             if str(e).startswith("Telegram says: [400 CHAT_FORWARDS_RESTRICTED]"):
                 return await message.reply("Master, Copying forwarding not allowed in that chat so we cannot paste it.")
-            await message.reply(f"Error: {e}")
+            await message.reply(f"Error: {e}") we
 
 @Sophia.on_message(filters.command("ncpaste", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def no_caption_paste_msg(_, message):
@@ -53,8 +59,12 @@ async def no_caption_paste_msg(_, message):
     else:
         try:
             if message.reply_to_message:
-                return await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), caption="", reply_to_message_id=message.reply_to_message_id)
-            await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), caption="")
+                if await IS_ALBUM() == True:
+                    return await Sophia.copy_media_group(message.chat.id, await CHAT_ID(), await MSG_ID(), reply_to_message_id=message.reply_to_message_id, caption="")
+                return await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), reply_to_message_id=message.reply_to_message_id, caption="")
+            if await IS_ALBUM() == True:
+                return await Sophia.copy_media_group(message.chat.id, await CHAT_ID(), await MSG_ID(), caption="")
+            return await Sophia.copy_message(message.chat.id, await CHAT_ID(), await MSG_ID(), caption="")
         except Exception as e:
             if str(e).startswith("Telegram says: [400 CHAT_FORWARDS_RESTRICTED]"):
                 return await message.reply("Master, Copying forwarding not allowed in that chat so we cannot paste it.")
