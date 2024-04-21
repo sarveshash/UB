1ce64deeb2ed904d67c095282d8ce454227b300e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-21 08:38:13 +0530

    Update gbans.py

--- Sophia/plugins/gbans.py
+++ Sophia/plugins/gbans.py
@@ -26,8 +26,10 @@ async def gban(_, message):
     else:
         if len(message.command) < 2:
             return await message.reply_text("Reply a user or enter the user id to gban")
-        loading_msg = await message.reply("Gbaning....")
         id = int(message.text.split(None, 1)[1])
+        if id == message.from_user.id:
+            return await message.reply("You can't gban yourself!")
+        loading_msg = await message.reply("Gbaning....")
         async for dialog in Sophia.get_dialogs():
             if dialog.chat.type == enums.ChatType.SUPERGROUP or dialog.chat.type == enums.ChatType.GROUP:
                 try:
@@ -39,3 +41,40 @@ async def gban(_, message):
                         print(e)
         await loading_msg.delete()
         await message.reply(f"Gban completed in {success_chats} chats\nTaken time: {int(time.time() - time_start)}")
+
+@Sophia.on_message(filters.command("ungban", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def ungban(_, message):
+    time_start = time.time()
+    success_chats = 0
+    if message.reply_to_message:
+        loading_msg = await message.reply("Ungbaning....")
+        async for dialog in Sophia.get_dialogs():
+            if dialog.chat.type == enums.ChatType.SUPERGROUP or dialog.chat.type == enums.ChatType.GROUP:
+                try:
+                    await Sophia.unban_chat_member(dialog.chat.id, message.reply_to_message.from_user.id)
+                    success_chats += 1
+                    await asyncio.sleep(2)
+                except Exception as e:
+                    if not str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""" and not str(e).startswith("Telegram says: [420 FLOOD_WAIT_X] - A wait"):
+                        print(e)
+        await loading_msg.delete()
+        await message.reply(f"Ungban completed in {success_chats} chats\nTaken time: {int(time.time() - time_start)}")
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text("Reply a user or enter the user id to ungban")
+        id = int(message.text.split(None, 1)[1])
+        if id == message.from_user.id:
+            return await message.reply("You can't ungban yourself!")
+        loading_msg = await message.reply("Ungbaning....")
+        async for dialog in Sophia.get_dialogs():
+            if dialog.chat.type == enums.ChatType.SUPERGROUP or dialog.chat.type == enums.ChatType.GROUP:
+                try:
+                    await Sophia.unban_chat_member(dialog.chat.id, id)
+                    success_chats += 1
+                    await asyncio.sleep(2)
+                except Exception as e:
+                    if not str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""" and not str(e).startswith("Telegram says: [420 FLOOD_WAIT_X] - A wait"):
+                        print(e)
+        await loading_msg.delete()
+        await message.reply(f"Ungban completed in {success_chats} chats\nTaken time: {int(time.time() - time_start)}")
+        
