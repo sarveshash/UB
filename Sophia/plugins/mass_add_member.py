ca62305784060c24c7485d218edd5d44326e9fcf
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-04-21 17:13:26 +0530

    Update mass_add_member.py

--- Sophia/plugins/mass_add_member.py
+++ Sophia/plugins/mass_add_member.py
@@ -7,22 +7,21 @@ import time
 from pyrogram import enums
 
 @Sophia.on_message(filters.command("madd", prefixes=HANDLER) & filters.user(OWNER_ID))
-async def gban(_, message):
+async def mass_add(_, message):
+    if len(message.command) < 2:
+        return await message.reply("Please enter group user name to mass add.")
     time_start = time.time()
     success = 0
-    if message.reply_to_message:
-        loading_msg = await message.reply("Adding member...")
-        async for dialog in Sophia.get_dialogs():
-            if dialog.chat.type == enums.ChatType.SUPERGROUP or dialog.chat.type == enums.ChatType.GROUP:
-                try:
-                    chat = dialog.chat
-                    await chat.add_members(message.reply_to_message.from_user.id)
-                    success += 1
-                    await asyncio.sleep(2)
-                except Exception as e:
-                    if not str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""" and not str(e).startswith("Telegram says: [420 FLOOD_WAIT_X] - A wait"):
-                        print(e)
-        await loading_msg.delete()
-        await message.reply(f"Successfully added in {success_chats} chats\nTaken time: {int(time.time() - time_start)}")
-    else:
-        await message.reply("Please reply to a user to mass adding")
+    chat_username = message.text.split(None, 1)[1]
+    loading_msg = await message.reply("Adding members...")
+    async for member in Sophia.get_chat_members(message.chat.id):
+        try:
+            output = await Sophia.add_chat_members(chat_username, member.user.id)
+            if output == True:
+                success += 1
+            await asyncio.sleep(2)
+        except Exception as e:
+            if not str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""" and not str(e).startswith("Telegram says: [420 FLOOD_WAIT_X] - A wait"):
+                print(e)
+    await loading_msg.delete()
+    await message.reply(f"Successfully added {success} members\nTaken time: {int(time.time() - time_start)}")
