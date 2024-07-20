65ef702c371a66b8ecffac32c366ae43decf41f9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-07-20 20:55:50 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -16,64 +16,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# IDK
-HM = "h"
-if HM == None:
-    OLD_DATAS_DB = input("Enter your mongodb uri: ") # For getting user values
-    DATABASE = MongoClient(OLD_DATAS_DB)["SOPHIA_UB"]
-    db = DATABASE["USER_DATAS"]
-    Find = db.find_one({"_id": 404})
-    if not Find:
-        VALUES = None
-    else:
-        VALUES = Find
-    if VALUES is not None:
-        count = 0
-        def inc_count():
-            global count
-            count += 1
-        for ai_rule_world in VALUES:
-            if count == 0:
-                r(f"export SESSION={ai_rule_world}")
-                inc_count()
-            elif count == 1:
-                r(f"export API_ID={ai_rule_world}")
-                inc_count()
-            elif count == 2:
-                r(f"export API_HASH={ai_rule_world}")
-                inc_count()
-            elif count == 3:
-                r(f"export MONGO_DB_URI={OLD_DATAS_DB}")
-                inc_count()
-            elif count == 4:
-                r(f"export YOUR_REPO_LINK={ai_rule_world}")
-                inc_count()
-                rs_pg()
-    while VALUES is None:
-        try:
-            while True:
-                SESSION = input("Enter you Pyrogram V-2 Session: ")
-                if len(SESSION) < 50:
-                    print("[Sophia System] Enter session correctly!")
-                else:
-                    break
-            API_ID = input("Enter Api ID: ")
-            API_HASH = input("Enter Api hash: ")
-            MONGO_DB_URI = input("Enter MONGO_DB_URI: ")
-            YOUR_REPO_LINK = input("Enter your forked repo link: ")
-            input("Enter to proceed: ")
-            
-            DATABASE = AsyncIOMotorClient(OLD_DATAS_DB)["SOPHIA_UB"]
-            db = DATABASE["USER_DATAS"]
-            doc = {"_id": 404, "SESSION": SESSION, "API_ID": API_ID, "API_HASH": API_HASH, "YOUR_REPO_LINK": YOUR_REPO_LINK, "everything": True}
-            try:
-                db.insert_one(doc)
-            except Exception:
-                db.update_one({"_id": 404}, {"$set": {"SESSION": SESSION, "API_ID": API_ID, "API_HASH": API_HASH, "YOUR_REPO_LINK": YOUR_REPO_LINK, "everything": True}})
-            break
-        except Exception as e:
-            print(e)
-
 # VARIABLES
 
 SESSION = os.environ.get("SESSION")
