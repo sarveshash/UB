7eacee7c0d382710b8b3d6e757f55c410a6d54ae
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-07-26 19:25:01 +0530

    Update shell.py

--- Sophia/plugins/shell.py
+++ Sophia/plugins/shell.py
@@ -9,24 +9,24 @@ import os
 import io
 
 @Sophia.on_message(filters.command(["sh", "shell", "bash"], prefixes=HANDLER) & ~filters.bot)
-def shell(_, message):
+async def shell(_, message):
     if message.from_user.id == OWN or message.from_user.id in loyal:
         print("")
     else:
         return
     if len(message.command) < 2:
-        message.edit("Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴄᴏᴅᴇ ᴛᴏ ʀᴜɴ ɪᴛ. 🥀 ✨")
+        await message.edit("Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴄᴏᴅᴇ ᴛᴏ ʀᴜɴ ɪᴛ. 🥀 ✨")
         return
     code = message.text.split(None, 1)[1]
-    message_text = message.reply_text("Pʀᴏᴄᴇssɪɴɢ...")
-    output = run(code)
+    message_text = await message.reply_text("Pʀᴏᴄᴇssɪɴɢ...")
+    output = await run(code)
     if len(output) > 4096:
         with io.BytesIO(str.encode(output)) as out_file:
             out_file.name = "shell.txt"
-            message.reply_document(
+            await message.reply_document(
                 document=out_file, disable_notification=True
             )
-            message_text.delete()
+            await message_text.delete()
     else:
-        message_text.edit(f"Oᴜᴛᴘᴜᴛ:\n`{output}`")
+        await message_text.edit(f"Oᴜᴛᴘᴜᴛ:\n`{output}`")
     
