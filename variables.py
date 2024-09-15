dbf69e3125c8bbd9758e138db6f8e147eb83ecfb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-15 16:48:48 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
-VAR_SESSION = ""
-VAR_API_ID = ""
-VAR_API_HASH = ""
+VAR_SESSION = "BQCbcXYAHL833uSuDGlOUZlMdv_I06iRuFLpuaANM3omQxmlzbCzU8f4XyBbkeiZXFITIHbpbyz-Vkx_TEyjqJ2VQf0AwYT5MTBDwTzQaZoKUWM3DrJYPOFZCLRek_Wy3BwGldQiH4TlmRdBMt78kj-NvF315BhOoAOwQSiSpmF8scxDsS-GO7EkpKFDrN1yvhSP7RQyZjA8ytYMeSZXBQ-Qr2OTpsT1BzG0ALr2z1kXi_Hf_6B4L25gID2PxWmBUhi4m_9IfUIsCAPVZfVb0DJjpngz7wHzhs8HItKtzX7Q0u2Sf8cZBgr0MW9KO4NjpjydTYmsgj_EoDAvPgjsEuNHsTDz7QAAAAFji4RfAA"
+VAR_API_ID = "10187126"
+VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
 VAR_MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
 VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
