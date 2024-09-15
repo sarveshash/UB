39fffe614e6f3b67d82ab78b605d579731a3120d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-15 16:43:14 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
-SESSION = ""
-API_ID = ""
-API_HASH = ""
-MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
-REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
+VAR_SESSION = ""
+VAR_API_ID = ""
+VAR_API_HASH = ""
+VAR_MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
+VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
