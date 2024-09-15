5a16b6bffaf4ea71256d0a83ba40e644fd9d2c41
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-15 16:24:11 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,7 @@
+# IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
+
+SESSION = ""
+API_ID = ""
+API_HASH = ""
+MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
+REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
