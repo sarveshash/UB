3f734cc730ed73b9dbded1456f03c72271d33b2c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 21:22:57 +0530

    Create start_message.py

---
+++ Sophia/plugins/start_message.py
@@ -0,0 +1,18 @@
+from Sophia import *
+from pyrogram import Client, filters
+import os
+import io
+from Sophia.Database.update import UPDATE
+import pyrogram
+from subprocess import getoutput as run
+
+async def start_message():
+    update_chk = UPDATE()
+    update_chk = update_chk.GET()
+    if update_chk is not False:
+        Sophia.send_message(update_chk, "Successfully Updated")
+        update_chk = UPDATE()
+        update_chk.ADD(False, 0)
+    Sophia.send_messags("me", "System Started")
+
+start_message()
