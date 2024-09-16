c04e56c855c10cd72edfb8ed571e4a3dfcfd05e8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 19:58:12 +0530

    Update update.py

--- Sophia/plugins/update.py
+++ Sophia/plugins/update.py
@@ -2,13 +2,17 @@ from pyrogram import filters
 from Sophia.__main__ import Sophia
 from Sophia import REPO_URL, repo_name, HANDLER
 from config import OWNER_ID
-from subprocess import getoutput as run
+import subprocess
 
 OWN = OWNER_ID
 
 @Sophia.on_message(filters.command("update", prefixes=HANDLER) & filters.user(OWN))
 async def update_repo(_, message):
     await message.reply_text("`Updating...`")
-    result = await run(f"cd && rm -rf {repo_name} && git clone {repo_name}  && cd {repo_name} && ls && python3 -m Sophia")
-    await message.reply_text(f"Update Failed: {result}")
-    print("Error on Updating", e)
+    try:
+        command = f"cd && rm -rf {repo_name} && git clone {REPO_URL} && cd {repo_name} && ls && python3 -m Sophia"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        exit()
+    except Exception as e:
+        await message.reply_text("Update Failed ", e)
+        print("Error on Updating", e)
