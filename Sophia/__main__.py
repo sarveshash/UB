18769bccbc5e1aab3c4ac252b69c8e01e47fc8c5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 20:45:52 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -10,9 +10,14 @@ from Restart import restart_program
 PWD = f"{os.getcwd()}/"
 
 if __name__ == "__main__":
+    Sophia.start()
     try:
-        Sophia.start()
-        Sophia.send_message("me", "hi da good")
-    except Exception as e:
-        print(e)
-    Sophia.run()
+        # START MESSAGE
+        update_chk = UPDATE()
+        update_chk = update_chk.GET()
+        if update_chk is not False:
+            Sophia.send_message(update_chk, "Successfully Updated")
+            update_chk = UPDATE()
+            update_chk.ADD(False, 0)
+        Sophia.send_messags("me", "System Started")
+    Sophia.idle()
