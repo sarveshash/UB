466bde47b7944ba1224e4c62634198a0f389dfb0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 16:22:17 +0530

    Update update.py

--- Sophia/plugins/update.py
+++ Sophia/plugins/update.py
@@ -9,6 +9,6 @@ OWN = OWNER_ID
 @Sophia.on_message(filters.command("update", prefixes=HANDLER) & filters.user(OWN))
 async def update_repo(_, message):
     await message.reply_text("`Updating...`")
-    result = await run(f"cd && rm -rf restarter && rm -rf {repo_name} && git clone {repo_name}  && cp -r {repo_name} restarter && cd {repo_name} && ls && python3 -m Sophia")
+    result = await run(f"cd && rm -rf {repo_name} && git clone {repo_name}  && cd {repo_name} && ls && python3 -m Sophia")
     await message.reply_text(f"Update Failed: {result}")
     print("Error on Updating", e)
