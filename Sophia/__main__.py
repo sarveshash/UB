a6a69a224022fd4f8ade15c5a847269ddb8b2a63
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 12:39:31 +0530

    Downgrade

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -9,13 +9,4 @@ from Restart import restart_program
 PWD = f"{os.getcwd()}/"
 
 if __name__ == "__main__":
-    try:
-        Sophia.run()
-    except Exception as e:
-        print("Crashed:", e)
-        print('Searching for backup file')
-        try:
-            run("cd && mv backup SophiaUB && cd SophiaUB && echo Update was failed Launching backup file && python3 -m Sophia")
-        except:
-            print("Backup file was not found")
-            exit()
+    Sophia.run()
