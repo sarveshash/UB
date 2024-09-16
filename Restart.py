0c867fe0538c04e68a0a6915460ae23999902941
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 16:25:37 +0530

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -4,9 +4,8 @@ from subprocess import getoutput as r
 
 def restart_program():
     try:
-        r("cd && cd restarter && python3 -m Sophia")
-    except:
         python = sys.executable
         script = os.path.abspath(sys.argv[0])
         os.execl(python, python, script, *sys.argv[1:])
-  
+    except:
+        r("cd && cp SophiaUB restarter && cd restartet && python3 -m Sophia")
