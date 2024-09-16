eb3644b1024f4a32a77797c7cb2fcf1cc6da6d1a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-16 21:23:54 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,7 +2,6 @@ from Sophia import *
 from pyrogram import Client, filters
 import os
 import io
-from Sophia.Database.update import UPDATE
 import pyrogram
 from subprocess import getoutput as run
 from Restart import restart_program
@@ -10,14 +9,4 @@ from Restart import restart_program
 PWD = f"{os.getcwd()}/"
 
 if __name__ == "__main__":
-    Start_message.start()
-    try:
-        # START MESSAGE
-        update_chk = UPDATE()
-        update_chk = update_chk.GET()
-        if update_chk is not False:
-            Start_message.send_message(update_chk, "Successfully Updated")
-            update_chk = UPDATE()
-            update_chk.ADD(False, 0)
-        Start_message.send_messags("me", "System Started")
     Sophia.run()
