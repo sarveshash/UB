24a8a5a00a76e12fb6a2171abacf66d5db3cb7bb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-17 11:19:19 +0530

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -1,9 +1,7 @@
 import sys
 import os
-from subprocess import getoutput as r
 
 def restart_program():
-    try:
         python = sys.executable
         script = os.path.abspath(sys.argv[0])
         os.execl(python, python, script, *sys.argv[1:])
