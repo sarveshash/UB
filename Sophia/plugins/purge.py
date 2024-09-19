3d3c611326cdc81819c7294a36ee91ee7508427b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-19 11:59:12 +0530

    Create purge.py

---
+++ Sophia/plugins/purge.py
@@ -0,0 +1,19 @@
+try:
+    from pyrogram import filters
+    from Sophia.__main__ import Sophia
+    from Sophia import HANDLER
+
+    @Sophia.on_message(filters.command("purge", prefixes=HANDLER) & filters.user("me"))
+    async def purge_messages(_, message):
+        if not message.reply_to_message:
+            return await message.reply("Reply to the first message you want to delete.")
+        start_msg_id = message.reply_to_message.message_id
+        end_msg_id = message.id
+        success = 0
+        for msg_id in range(start_msg_id, end_msg_id + 1):
+            try:
+                await Sophia.delete_messages(message.chat.id, msg_id)
+                success +1
+            except Exception as e:
+                print(f"Error deleting message {msg_id}: {str(e)}")
+        await message.reply(f"Successfuly Purged {success} messages!")
