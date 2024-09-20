5b9dfd090fc51960fe9f1f11da14a6af7b2d9d6e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-20 11:52:02 +0530

    Update shell.py

--- Sophia/plugins/shell.py
+++ Sophia/plugins/shell.py
@@ -1,19 +1,14 @@
 from Sophia import HANDLER
 from Sophia.__main__ import Sophia
 from config import OWNER_ID as OWN
-from config import SUDO_USERS_ID as loyal
 from pyrogram import filters
 from subprocess import Popen, PIPE
 import asyncio
 import os
 import io
 
-@Sophia.on_message(filters.command(["sh", "shell", "bash"], prefixes=HANDLER) & ~filters.bot)
+@Sophia.on_message(filters.command(["sh", "shell", "bash"], prefixes=HANDLER) & filters.user('me'))
 async def shell(_, message):
-    if message.from_user.id == OWN or message.from_user.id in loyal:
-        print("")
-    else:
-        return
 
     process = None
 
@@ -29,8 +24,15 @@ async def shell(_, message):
                     pass
 
     async def get_input():
-        response = await Sophia.listen(filters.chat(message.chat.id) & filters.user("me"))
-        return response.text
+        my_filter = filters.create(lambda _, __, message: 
+                               message.chat.id == message.chat.id and 
+                               message.from_user.id == "me")
+
+        try:
+            response = await asyncio.wait_for(Sophia.wait_for_message(my_filter), timeout=60)
+            return response.text
+        except asyncio.TimeoutError:
+            return None
 
     if len(message.command) < 2:
         await message.edit("Mᴀsᴛᴇʀ, Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴄᴏᴅᴇ ᴛᴏ ʀᴜɴ ɪᴛ. 🥀 ✨")
@@ -39,7 +41,6 @@ async def shell(_, message):
     message_text = await message.reply_text("Pʀᴏᴄᴇssɪɴɢ...")
 
     process = Popen(code, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True, bufsize=1)
-
     asyncio.create_task(process_output(process, message_text))
 
     while process.poll() is None:
@@ -52,3 +53,4 @@ async def shell(_, message):
         await message_text.edit(f"Error:\n`{error}`")
     else:
         await message_text.edit(f"Oᴜᴛᴘᴜᴛ:\n`{output.strip()}`")
+        
