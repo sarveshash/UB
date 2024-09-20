9ee96cd244f6134e1ae49c949d7db4ac56b175a4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-20 23:18:12 +0530

    convert_class.py

---
+++ Sophia/plugins/convert_class.py
@@ -0,0 +1,37 @@
+from Sophia import HANDLER, Sophia
+from pyrogram import filters
+from subprocess import getoutput as run
+import os
+import io
+
+@Sophia.on_message(filters.document & filters.user('me'))
+async def translate_java(_, message):
+    document = message.document
+    if not document.file_name.endswith(".class"):
+        return
+    message_text = await message.reply_text("Processing...")
+    class_file_path = await message.download()
+    if not os.path.exists("cfr.jar"):
+        download_output = run("wget https://www.benf.org/other/cfr/cfr-0.152.jar -O cfr.jar")
+        if "error" in download_output.lower():
+            return await message_text.edit(f"Error downloading cfr.jar: {download_output}")
+    reverse_output = run(f"java -jar cfr.jar --renameillegalidents true --renamedupmembers true {class_file_path} && java -jar cfr.jar {class_file_path}")
+    if "error" in reverse_output.lower():
+        return await message_text.edit(f"Error during bytecode reversal: {reverse_output}")
+    java_file_path = "MyProgram.java"
+    with open(java_file_path, "w") as java_file:
+        java_file.write(reverse_output)
+    with open(java_file_path, "r") as java_file:
+        java_code = java_file.read()
+    os.remove(java_file_path)
+    try:
+        os.remove(class_file_path)
+    except FileNotFoundError:
+        pass
+    if len(java_code) > 4096:
+        with io.BytesIO(str.encode(java_code)) as out_file:
+            out_file.name = "java_output.txt"
+            await message.reply_document(document=out_file, disable_notification=True)
+            await message_text.delete()
+    else:
+        await message_text.edit(f"```java\n// This code may not work properly\n{java_code}")
