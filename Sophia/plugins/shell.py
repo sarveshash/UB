ffa8fa35b43da85d6f99d11c9b845c4b8f4a091f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-20 12:49:18 +0530

    Update shell.py

--- Sophia/plugins/shell.py
+++ Sophia/plugins/shell.py
@@ -1,45 +1,24 @@
-from Sophia import *
+from Sophia import HANDLER
+from Sophia.__main__ import Sophia
 from pyrogram import filters
+from subprocess import getoutput as run
 import asyncio
+import os
 import io
 
-user_shells = {}
-
-async def run_command(shell, command):
-    shell.stdin.write(f"{command}\n".encode())
-    await shell.stdin.drain()
-    output = await shell.stdout.readuntil(b'\n')
-    return output.decode()
-
-@Sophia.on_message(filters.command("sh", prefixes=HANDLER) & filters.user("me"))
-async def start_shell(_, message):
-    user_id = message.from_user.id
-    if user_id not in user_shells:
-        shell = await asyncio.create_subprocess_exec(
-            "/bin/bash", "-i",
-            stdin=asyncio.subprocess.PIPE,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        user_shells[user_id] = shell
-    command = str(message.text.split(None, 1)[1])
-    shell = user_shells[user_id]
-    try:
-        output = await run_command(shell, command)
-    except Exception as e:
-        print("Error:", e)
-        return await message.reply_text(f"Error running command: {e}")
+@Sophia.on_message(filters.command(["sh", "shell", "bash"], prefixes=HANDLER) & filters.user('me'))
+async def shell(_, message):
+    if len(message.command) < 2:
+        return await message.edit("Please enter a command to run! 🥀 ✨")
+    code = message.text.split(None, 1)[1]
+    message_text = await message.reply_text("`Processing...`")
+    output = run(code)
     if len(output) > 4096:
         with io.BytesIO(str.encode(output)) as out_file:
-            out_file.name = "command_output.txt"
-            await message.reply_document(out_file)
+            out_file.name = "shell.txt"
+            await message.reply_document(
+                document=out_file, disable_notification=True
+            )
+            await message_text.delete()
     else:
-        await message.reply_text(f"Output:\n`{output}`")
-
-
-@Sophia.on_message(filters.command("esh", prefixes=HANDLER) & filters.user("me"))
-async def exit_shell(_, message):
-    user_id = message.from_user.id
-    shell = user_shells.pop(user_id)
-    shell.terminate()
-    await message.reply_text("Live shell session closed.")
+        await message_text.edit(f"**Output**:\n`{output}`")
