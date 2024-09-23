9e70b9d0b73f648c9a25502f10e2c804a7a7deae
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-23 16:16:06 +0530

    Update purge.py

--- Sophia/plugins/purge.py
+++ Sophia/plugins/purge.py
@@ -14,13 +14,21 @@ try:
         start_msg_id = message.reply_to_message.id
         end_msg_id = message.id
         success = 0
+        breaked = False
         for msg_id in range(start_msg_id, end_msg_id + 1):
             try:
                 await Sophia.delete_messages(message.chat.id, msg_id)
                 success += 1
                 await asyncio.sleep(0.3)
+                if success >= 100:
+                    break
+                    breaked = True
             except Exception as e:
                 print(f"Error deleting message {msg_id}: {str(e)}")
+        if breaked:
+            await message.reply("Successfuly purged 100 messages, I can only delete 100 try again!")
+            breaked = False
+            return
         await message.reply(f"Successfuly purged {success} messages!")
 except Exception as e:
     e = f"Error on purge.py: {e}"
