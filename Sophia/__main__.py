93fa568448955acb59abefd49a40d69b666585f6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-25 12:05:38 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,6 +2,7 @@ from Sophia import *
 from pyrogram import Client, filters
 import os
 import io
+from pytgcalls import *
 import pyrogram
 from subprocess import getoutput as run
 from Restart import restart_program
@@ -11,4 +12,6 @@ PWD = f"{os.getcwd()}/"
 
 if __name__ == "__main__":
     run("apt update && apt install -y default-jre && apt install -y default-jdk && apt install -y nodejs npm && nvm install node && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash")
+    Friday.idle()
+    print("Friday started!")
     Sophia.run()
