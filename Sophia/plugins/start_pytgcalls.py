9289b487eaeff080e23a76f0fe876454cc58a7eb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-25 13:00:35 +0530

    Update start_pytgcalls.py

--- Sophia/plugins/start_pytgcalls.py
+++ Sophia/plugins/start_pytgcalls.py
@@ -9,6 +9,13 @@ async def start_pytgcalls(_, message):
     global started
     if started:
         return await message.reply("Already pytgcalls client running!")
-    await message.reply("Successfuly started pytgcalls client!")
-    started = True
-    idle()
+    mano = await message.reply("`Starting pytgcalls client...`")
+    try:
+        await Friday.start()
+        await mano.edit("Successfuly started pytgcalls!")
+        started = True
+        await idle()
+    except Exception as e:
+        started = False
+        await mano.edit(f"Error: {e}")
+        print(f"Somthing went wrong while starting PyTgcalls: {e}")
