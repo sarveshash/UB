e89c3696db0a435ccd9de85fc8423535df27ae2b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-25 12:41:45 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,7 +2,6 @@ from Sophia import *
 from pyrogram import Client, filters
 import os
 import io
-from pytgcalls import *
 import pyrogram
 from subprocess import getoutput as run
 from Restart import restart_program
@@ -13,5 +12,4 @@ PWD = f"{os.getcwd()}/"
 if __name__ == "__main__":
     run("apt update && apt install -y ffmpeg")
     # run("apt install -y default-jre && apt install -y default-jdk && apt install -y nodejs npm && nvm install node && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash")
-    print("Friday started!")
     Sophia.run()
