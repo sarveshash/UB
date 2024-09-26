02a1009f5f09b9f7507be5dad2e3371925ad6487
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-26 11:29:57 +0530

    Update new_afk.py

--- Sophia/plugins/new_afk.py
+++ Sophia/plugins/new_afk.py
@@ -5,6 +5,7 @@ from Sophia.Database.ignore_users import *
 from Sophia.Database.backup_msg import *
 from Sophia.Database.pmguard import *
 from pyrogram.enums import *
+from config import OWNER_ID
 import os
 import re
 import asyncio
@@ -16,6 +17,21 @@ async def filter_(_, client, update):
             if update.text:
                 if update.text.startswith(tuple(HANDLER)) and not len(update.text) < 2:
                     command = True
-        if not command and update.chat.id not in await GET_STOP_BACKUP_CHATS():
-            
+        if not command and update.chat.id not in await GET_STOP_BACKUP_CHATS() and update.chat.id != OWNER_ID:
+            CHATS = await GET_BACKUP_CHATS()
+            if update.chat.id in CHATS:
+                chat_id = await GET_BACKUP_CHANNEL_ID(update.chat.id)
+                try:
+                    await Sophia.forward_messages(chat_id, message.chat.id, message.id)
+                except:
+                    if message.chat.username:
+                        chat = await Sophia.create_channel(f"{message.chat.first_name} BACKUP", f"Username: @{message.chat.username}\n\n~ @Hyper_Speed0")
+                    else:
+                        chat = await Sophia.create_channel(f"{message.chat.first_name} BACKUP", "~ @Hyper_Speed0")
+                    await ADD_BACKUP_CHAT(message.chat.id)
+                    await SET_BACKUP_CHANNEL_ID(message.chat.id, chat.id)
+                    await Sophia.forward_messages(chat.id, message.chat.id, message.id)
+                    await Sophia.archive_chats(chat.id)
+            else:
+                
             
