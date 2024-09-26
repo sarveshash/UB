7c679779c7eaa97d03787407c4072f500f926cee
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-26 11:35:23 +0530

    Update new_afk.py

--- Sophia/plugins/new_afk.py
+++ Sophia/plugins/new_afk.py
@@ -10,7 +10,16 @@ import os
 import re
 import asyncio
 
+def calculate_time(start_time, end_time):
+    ping_time = (end_time - start_time).total_seconds() * 1000
+    uptime = (end_time - start_time).total_seconds()
+    hours, remainder = divmod(uptime, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    time = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+    return time
+
 async def filter_(_, client, update):
+    message = update
     if await GET_BACKUP(): # Backup section
         command = False
         if update.from_user.id == OWNER_ID:
@@ -33,5 +42,13 @@ async def filter_(_, client, update):
                     await Sophia.forward_messages(chat.id, message.chat.id, message.id)
                     await Sophia.archive_chats(chat.id)
             else:
-                
+                if message.chat.username:
+                    chat = await Sophia.create_channel(f"{message.chat.first_name} BACKUP", f"Username: @{message.chat.username}\n\n~ @Hyper_Speed0")
+                else:
+                    chat = await Sophia.create_channel(f"{message.chat.first_name} BACKUP", "~ @Hyper_Speed0")
+                await ADD_BACKUP_CHAT(message.chat.id)
+                await SET_BACKUP_CHANNEL_ID(message.chat.id, chat.id)
+                await Sophia.forward_messages(chat.id, message.chat.id, message.id)
+                await Sophia.archive_chats(chat.id)
+   f 
             
