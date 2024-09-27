fcc74670df374f7975e1303c6488e0053ae8f97b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-27 11:41:08 +0530

    Update delete_chat.py

--- Sophia/plugins/delete_chat.py
+++ Sophia/plugins/delete_chat.py
@@ -5,12 +5,12 @@ from config import OWNER_ID
 
 @Sophia.on_message(filters.command("delchat", HANDLER) & filters.user('me'))
 async def delete_chat(_, m):
-  if not message.chat.type == ChatType.PRIVATE:
+  if not m.chat.type == ChatType.PRIVATE:
     return await m.reply("Please use this command on private chat!")
-  if message.chat.id == OWNER_ID:
+  if m.chat.id == OWNER_ID:
     return await m.reply("Sorry you cannot use this here!")
   try:
-    await Sophia.delete_chat_history(m.chat.id)
+    await Sophia.delete_chat_history(m.chat.id, revoke=True)
     await m.reply("Successfuly deleted the chat!")
   except Exception as e:
     print(f"Something went wrong while deleting chat on {m.chat.id}: {e}")
