5489443a084779f877c5970d7dbe0662d16c8f68
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-09-28 17:42:54 +0530

    No more issues on this maybe

--- Sophia/plugins/message_handler.py
+++ Sophia/plugins/message_handler.py
@@ -39,14 +39,10 @@ async def filter_(_, client, update):
                     elif message.chat.type == ChatType.PRIVATE:
                         await Sophia.forward_messages(chat_id, message.chat.id, message.id)
                 except Exception as e:
-                    if not str(e).startswith(tuple([
-                        "Telegram says: [400 PEER_ID_INVALID]", 
-                        "Telegram says: [406 CHANNEL_PRIVATE]", 
-                        "Telegram says: [400 CHANNEL_INVALID]", 
+                    if str(e).startswith(tuple([
+                        "Telegram says: [400 CHAT_FORWARDS_RESTRICTED", 
                         "Telegram says: [400 MESSAGE_ID_INVALID]"
                     ])):
-                        if not str(e).startswith("Telegram says: [400 CHAT_FORWARDS_RESTRICTED"):
-                            await Sophia.send_message('me', f'Error while forwarding backup message to channel in {update.chat.id}: {e}')
                         return False
                     if message.chat.first_name is not None:
                         c_name = f"{message.chat.first_name} BACKUP"
