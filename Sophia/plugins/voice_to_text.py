db98024831bf31dbc6e6b67c3f64cc5c4d1d352e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-22 19:18:15 +0530

    Update voice_to_text.py

--- Sophia/plugins/voice_to_text.py
+++ Sophia/plugins/voice_to_text.py
@@ -1,15 +1,19 @@
 from Sophia import *
 from pyrogram import *
-from pydub import AudioSegment
+import ffmpeg
 import speech_recognition as sr
 from subprocess import getoutput as r
 import time
 from pyrogram import enums
+import os
 
 @Sophia.on_message(filters.command('vtt', prefixes=HANDLER) & filters.user('me'))
 async def voice_to_text(_, message):
     if not message.reply_to_message:
         return await message.reply('Please reply to an audio file to convert')
+    
+    os.makedirs("voice_convert", exist_ok=True)
+    
     if message.reply_to_message.media == enums.MessageMediaType.VOICE:
         await message.reply_to_message.download(file_name="voice_convert/output.mp3")
     elif message.reply_to_message.audio:
@@ -18,15 +22,21 @@ async def voice_to_text(_, message):
         await message.reply_to_message.download(file_name="voice_convert/output.mp3")
     else:
         return await message.reply("Please reply to a valid audio file!")
+    
     edit_message = await message.reply("Converting audio to text...")
-    audio = AudioSegment.from_file("voice_convert/output.mp3")
-    audio.export("voice_convert/output_file.wav", format="wav")
+    
+    try:
+        ffmpeg.input("voice_convert/output.mp3").output("voice_convert/output_file.wav").run()
+    except ffmpeg.Error as e:
+        await edit_message.edit(f"Error converting file: {e}")
+        return
     
     recognizer = sr.Recognizer()
     audio_file = "voice_convert/output_file.wav"
     
     with sr.AudioFile(audio_file) as source:
         audio_data = recognizer.record(source)
+    
     retries = 3
     delay = 1
     for attempt in range(retries):
@@ -39,10 +49,9 @@ async def voice_to_text(_, message):
             break
         except sr.RequestError as e:
             if attempt < retries - 1:
-                await edit_message.edit(f"Error: {e}. Retrying, remaining {retries} attempt...")
+                await edit_message.edit(f"Error: {e}. Retrying, remaining {retries - attempt - 1} attempts...")
                 time.sleep(delay)
             else:
                 await message.reply(f"Failed to connect to Google Speech Recognition service after {retries} attempts.")
-                print(f"An error occurred on voice_to_text.py: {e}")
     
     await r("rm -rf voice_convert")
