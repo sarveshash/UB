7918de652d5a4d077bd8e60725ceb78c262b5c49
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-22 17:16:23 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -10,6 +10,4 @@ PWD = f"{os.getcwd()}/"
 
 
 if __name__ == "__main__":
-    run("apt update && apt install -y ffmpeg")
-    run("apt install -y default-jre && apt install -y default-jdk && apt install -y nodejs npm && nvm install node && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash")
     Sophia.run()
