968bd82ab5acddfd4428362db5e023159301ed1d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-23 16:09:59 +0530

    Update madd.py

--- Sophia/plugins/madd.py
+++ Sophia/plugins/madd.py
@@ -12,10 +12,13 @@ async def mass_add(_, message):
         return await message.reply("Please enter group user name to mass add.")
     time_start = time.time()
     success = 0
-    chat_username = message.text.split(None, 1)[1]
+    chat_username = str(message.text.split(None, 1)[1])
     chat_info = await Sophia.get_chat(chat_username)
+    if not chat_username.startswith(('@', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+        return await message.reply("Please enter a valid id.")
     if chat_info.type == ChatType.PRIVATE or chat_info.type == ChatType.CHANNEL:
         return await message.reply('Please enter a group id')
+        
     loading_msg = await message.reply("Adding members...")
     async for member in Sophia.get_chat_members(message.chat.id):
         try:
