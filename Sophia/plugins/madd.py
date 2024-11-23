044d4caf2a65067823d81da651f1efb27398af91
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-23 13:32:26 +0530

    Update and rename mass_add_member.py to madd.py

--- Sophia/plugins/madd.py
+++ Sophia/plugins/madd.py
@@ -4,15 +4,18 @@ from config import OWNER_ID
 from pyrogram import filters
 import asyncio
 import time
-from pyrogram import enums
+from pyrogram.enums import ChatType
 
-@Sophia.on_message(filters.command("madd", prefixes=HANDLER) & filters.user(OWNER_ID))
+@Sophia.on_message(filters.command("madd", prefixes=HANDLER) & filters.user(OWNER_ID) & filters.group)
 async def mass_add(_, message):
     if len(message.command) < 2:
         return await message.reply("Please enter group user name to mass add.")
     time_start = time.time()
     success = 0
     chat_username = message.text.split(None, 1)[1]
+    chat_info = await Sophia.get_chat(message.chat.id)
+    if chat_info.type == ChatType.PRIVATE or chat_info.type == ChatType.CHANNEL:
+        return await message.reply('Please enter a group id')
     loading_msg = await message.reply("Adding members...")
     async for member in Sophia.get_chat_members(message.chat.id):
         try:
@@ -25,4 +28,6 @@ async def mass_add(_, message):
             if not str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""" and not str(e).startswith("Telegram says: [420 FLOOD_WAIT_X] - A wait"):
                 print(e)
     await loading_msg.delete()
+    if success <= 0:
+        return await message.reply(f"Failed to add members ❌")
     await message.reply(f"Successfully added {success} members\nTaken time: {int(time.time() - time_start)}")
