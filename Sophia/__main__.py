8303e47ac50833a9621281620a2d50cda3c4a685
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-24 21:21:05 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -3,27 +3,31 @@ from Sophia import *
 from pyrogram import Client, filters
 import os
 import io
-import pyrogram
 from subprocess import getoutput as run
 from Restart import restart_program
 
 PWD = f"{os.getcwd()}/"
-my_id = None 
+my_id = None
+
 async def runn():
     global my_id
     await Sophia.start()
     my_id = await Sophia.get_me()
     my_id = my_id.id
+
     try:
         await Sophia.send_message(-1001859707851, "Sophia system started")
     except:
         await Sophia.join_chat(-1001859707851)
         await Sophia.send_message(-1001859707851, "Sophia system started")
+
     await Sophia.stop()
     return my_id
-        
 
+async def main():
+    my_id = await runn()
+    print(f"My ID is: {my_id}")
+    await Sophia.run()
 
 if __name__ == "__main__":
-    my_id = asyncio.run(runn())
-    Sophia.run()
+    asyncio.run(main())
