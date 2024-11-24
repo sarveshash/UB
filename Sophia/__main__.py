3eba392ea2fdfd3dde2eb9ae309568aa57568d08
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-24 21:46:31 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -9,26 +9,5 @@ from Restart import restart_program
 PWD = f"{os.getcwd()}/"
 my_id = None
 
-async def runn():
-    global my_id
-    await help_client.start()
-    my_id = await help_client.get_me()
-    my_id = my_id.id
-
-    try:
-        await help_client.send_message(-1001859707851, "Sophia system started")
-    except:
-        await help_client.join_chat(-1001859707851)
-        await help_client.send_message(-1001859707851, "Sophia system started")
-    await help_client.stop()
-    return my_id
-
-async def main():
-    task = asyncio.create_task(runn())
-    my_id = await task
-    task.cancel()
-    print(f"My ID is: {my_id}")
-    await Sophia.run()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    Sophia.run()
