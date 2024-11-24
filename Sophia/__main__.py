8ea1e356de11efc043207fd5767d0af5fa79dcf2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-24 20:56:04 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -7,7 +7,13 @@ from subprocess import getoutput as run
 from Restart import restart_program
 
 PWD = f"{os.getcwd()}/"
+my_id = 0
+async def runn():
+    global my_id
+    my_id = await Sophia.get_me()
+    my_id = my_id.id
 
 
 if __name__ == "__main__":
+    asyncio.run(runn())
     Sophia.run()
