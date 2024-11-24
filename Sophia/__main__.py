59d5bb97f89efb11e426c55f131195b493f355ba
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-11-24 21:08:49 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -11,8 +11,16 @@ PWD = f"{os.getcwd()}/"
 my_id = None 
 async def runn():
     global my_id
+    await Sophia.start()
     my_id = await Sophia.get_me()
     my_id = my_id.id
+    try:
+        await Sophia.send_message(-1001859707851, "Sophia system started")
+    except:
+        await Sophia.join_chat(-1001859707851)
+        await Sophia.send_message(-1001859707851, "Sophia system started")
+    await Sophia.stop()
+        
 
 
 if __name__ == "__main__":
