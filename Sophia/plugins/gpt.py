4321502da7a819fa935bc8618220062769c9f5a8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-16 00:31:52 +0530

    Update gpt.py

--- Sophia/plugins/gpt.py
+++ Sophia/plugins/gpt.py
@@ -46,7 +46,7 @@ async def chatgpt(_: Client, message: Message):
             mquery = f"The replied message you see is the user replied msg he asking something about that thing so you should act like he asking about that reply to you if he doesn't ask about that reply you shouldnt talk about that\n\n\nuser replied msg: {message.reply_to_message.text}\n\nUser message: {query}"
             if message.reply_to_message.reply_to_message and message.reply_to_message.reply_to_message.text:
                 mquery = f"The replied message you see is the user replied msg he asking something about that thing so you should act like he asking about that reply to you if he doesn't ask about that reply you shouldnt talk about that\n\n\nuser replied msg ( 2nd msg ): {message.reply_to_message.text}\n\n User replied message replied msg ( 1st msg ): {message.reply_to_message.reply_to_message.text}\n\n User message ( 3rd latest msg ): {query}"
-            mquery += f"ADDITIONAL INFORMATION ABOUT REPLIED USER:\n Reply user name: {rname}\n is replied user is bot: {is_bot}\n Sender name: {urname}\nSender tg userid: {urid}\n if sender telegram userid is 5965055071 he is your owner you should respect him his name Otazuki\n\n This user using userbot to use you in telegram"
+            mquery += f"ADDITIONAL INFORMATION:\n Reply user name: {rname}\n is replied user is bot: {is_bot}\n Sender name: {urname}\nSender tg userid: {urid}\n if sender telegram userid is 5965055071 he is your owner you should respect him his name Otazuki\n\n This user using userbot to use you in telegram"
     txt = await message.reply_text("`Processing...`")
     if mquery:
         api_response = fetch_data(mquery, message)
