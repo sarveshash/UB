a3fea539e853029b8e9096aea651a2b2e96e9772
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-15 20:28:31 +0530

    Update gpt.py

--- Sophia/plugins/gpt.py
+++ Sophia/plugins/gpt.py
@@ -27,18 +27,15 @@ def fetch_data(query: str, message: str) -> tuple:
         }
         response = requests.post(url, headers=headers, data=json.dumps(data))
         data = response.json()
-        return data.get("message", "No response from the API."), None
-    except requests.exceptions.RequestException as e:
-        return None, f"Request error: {e}"
+        return data.get("message", "No response from the API.")
     except Exception as e:
-        return None, f"An error occurred: {str(e)}"
+        return f"An error occurred: {str(e)}"
 
 @app.on_message(filters.command(["chat", "gpt"], prefixes=HANDLER) & filters.user(OWNER_ID))
 async def chatgpt(_: Client, message: Message):
     if len(message.command) < 2:
-        return await message.reply_text("Master, Please provide a query.")
-
+        return await message.reply_text("Please provide a query.")
     query = " ".join(message.command[1:])
     txt = await message.reply_text("`Processing...`")
-    api_response, error_message = fetch_data(query, message)
-    await txt.edit(api_response or error_message)
+    api_response = fetch_data(query, message)
+    await txt.edit(api_response)
