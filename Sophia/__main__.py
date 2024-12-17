b424f3769c66c9333273a4af328cc5d8a47c2363
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-17 19:32:32 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -5,9 +5,11 @@ import os
 import io
 from subprocess import getoutput as run
 from Restart import restart_program
+from pytgcalls import idle
 
 PWD = f"{os.getcwd()}/"
 my_id = None
 
 if __name__ == "__main__":
+    idle()
     Sophia.run()
