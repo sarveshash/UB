20a1077a301f67c670ce5bd202f11bd4f7cbc6cb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-25 17:41:06 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -14,14 +14,13 @@ if __name__ == "__main__":
     Sophia.send_photo(
         'me',
         photo="https://i.imgur.com/DuoscLX.jpeg",
-        caption=f"""
-        **✅ Sophia started ⚡**
-        
-        **👾 Version:** {MY_VERSION}
-        **🥀 Python:** 3.12
-        **🐬 Owner:** {Sophia.me.first_name if not Sophia.me.last_name else f'{Sophia.me.first_name} {Sophia.me.last_name}'}
-        **🦋 Join:** __@Hyper_speed0 & @FutureCity005__
-        """
+        caption=(
+            f"**✅ Sophia started ⚡**\n\n"
+            f"**👾 Version:** {MY_VERSION}\n"
+            f"**🥀 Python:** 3.12\n"
+            f"**🐬 Owner:** {Sophia.me.first_name if not Sophia.me.last_name else f'{Sophia.me.first_name} {Sophia.me.last_name}'}\n"
+            f"**🦋 Join:** __@Hyper_speed0 & @FutureCity005__"
+        )
     )
     SophiaBot.start()
     idle()
