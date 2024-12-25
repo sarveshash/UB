dedfeecd0a79fbe8c4d361ca80cf722be7e8de06
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-25 17:55:49 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -12,17 +12,18 @@ my_id = None
 if __name__ == "__main__":
     Sophia.start()
     try:
-        Sophia.send_photo(
-            -1001859707851,
-            photo="https://i.imgur.com/DuoscLX.jpeg",
-            caption=(
-                f"**✅ Sophia started ⚡**\n\n"
-                f"**👾 Version:** {MY_VERSION}\n"
-                f"**🥀 Python:** 3.12\n"
-                f"**🐬 Owner:** {Sophia.me.first_name if not Sophia.me.last_name else f'{Sophia.me.first_name} {Sophia.me.last_name}'}\n"
-                f"**🦋 Join:** __@Hyper_speed0 & @FutureCity005__"
+        if not 143 == 143:
+            Sophia.send_photo(
+                -1001859707851,
+                photo="https://i.imgur.com/DuoscLX.jpeg",
+                caption=(
+                    f"**✅ Sophia started ⚡**\n\n"
+                    f"**👾 Version:** {MY_VERSION}\n"
+                    f"**🥀 Python:** 3.12\n"
+                    f"**🐬 Owner:** {Sophia.me.first_name if not Sophia.me.last_name else f'{Sophia.me.first_name} {Sophia.me.last_name}'}\n"
+                    f"**🦋 Join:** __@Hyper_speed0 & @FutureCity005__"
+                )
             )
-        )
     except:
         pass
     SophiaBot.start()
