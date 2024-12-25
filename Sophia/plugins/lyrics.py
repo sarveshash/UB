df630fff9a2ff92f5c9b17ae77b7deb0cddb982e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-26 01:13:16 +0530

    Update lyrics.py

--- Sophia/plugins/lyrics.py
+++ Sophia/plugins/lyrics.py
@@ -24,11 +24,11 @@ async def lyrics(_, message):
           url = f"https://api.lyrics.ovh/v1/{artist_name}/{song_title}"
           async with session.get(url) as lyric:
             if lyric.status == 200:
-              lyric_data = await lyric.json()
+              lyric_data = await lyric.json()['lyrics']
               lyric_data = lyric_data.replace('\n', '')
               lyric_data = lyric_data.replace('\n\n', '')
               lyric_data = lyric_data.replace('\n\n\n', '\n')
-              await message.reply(f"**Lyrics of: {artist_name} - {song_title}**\n\n{lyric_data['lyrics']}")
+              await message.reply(f"**Lyrics of: {artist_name} - {song_title}**\n\n{lyric_data}")
             else:
               await message.reply("Couldn't find the song ❌")
         else:
