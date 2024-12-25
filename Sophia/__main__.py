06d6fac491d2633956764e0b5a107cf04df543af
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-25 17:11:09 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -2,6 +2,7 @@ import threading
 from Sophia import *
 from pyrogram import Client, filters
 import os
+from pyrogram import idle
 from subprocess import getoutput as run
 from Restart import restart_program
 
@@ -9,4 +10,6 @@ PWD = f"{os.getcwd()}/"
 my_id = None
 
 if __name__ == "__main__":
-    Sophia.run()
+    Sophia.start()
+    SophiaBot.start()
+    idle()
