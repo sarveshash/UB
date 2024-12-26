2e551e8a7adedfae3c3f2c2dd84e6d4d3adf8922
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-26 12:40:41 +0530

    Update restart.py

--- Sophia/plugins/restart.py
+++ Sophia/plugins/restart.py
@@ -11,4 +11,7 @@ async def Call_Restart_Func(_, message):
     await message.edit("`Killing all process`")
     await asyncio.sleep(0.34)
     await message.edit("`All process killed shuting down`")
+    with open("others/restarted.py", "w") as mano:
+        ctx = f"is_restarted = True\nrestart_msg = {message}"
+        mano.write(ctx)
     restart_program()
