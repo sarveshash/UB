71ee3fab5e518ec21d2e28da90e155e0391fbd84
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-26 13:30:53 +0530

    Update play.py

--- Sophia/Database/play.py
+++ Sophia/Database/play.py
@@ -9,18 +9,19 @@ class play:
     async def get(self):
         try:
             info = await db.find_one({"_id": 1})
-            return info or ''
+            return info or []
         except Exception as e:
             logging.error(e)
             return str(e)
     async def addRemove(self, chat_id, addOrRemove='add'):
         try:
             if addOrRemove == 'add':
-                info = await db.find_one({"_id": 1})
+                info = await db.find_one({"_id": 1}) or []
                 if chat_id in info:
                     return 'ALREADY'
                 await db.update_one({"_id": 1}, {"$addToSet": {"chats": chat_id}})
             elif addOrRemove == 'remove':
+                info = await db.find_one({"_id": 1}) or []
                 if chat_id in info:
                     await db.update_one({"_id": 1}, {"$pull": {"chats": chat_id}})
                 else: return 'ALREADY'
