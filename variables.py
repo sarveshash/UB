ddadda058b9b22215e1070c1f5396084028be5f5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-26 20:56:53 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@
 VAR_SESSION = ""
 VAR_API_ID = "10187126"
 VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
-VAR_MONGO_DB_URI = ""
+VAR_MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 VAR_TOKEN = "BQCbcXYAiSQG7bAeyY1tR0zVoyTOhfU7LROEIvP_EGYpCWKq8-OO5HwZYjRIqS7mkrJPj6adatyD-cHS1O-b_q05wiuLrG1Fu508NdFBaB53qIQpTcVcjxl1Mz28Fc6E8qcR45SBxd3RjQ4uyU9rscNKcYTixH0LFmRoR9nBm-hcY6JnZ-FsYbgNUbXMjmMox_wmWIewAwUMSG3GXx7xUdQhlpk1KqjQedBFL2Was3mISV1-jf9uDGOL32gnQVUAg2z5JVUrqzXDbQc4niLgVg3GT1n4PTl27GCu5fpXwepbmAtxm7aTC255agwLHu8XN7CygOocYLY4mbd0UiUEELDIZxmFogAAAAGb5MbNAQ"
 VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
 
