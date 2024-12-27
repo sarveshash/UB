6c3e111d1520f425555ac0e4bc9dd87512feaf54
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-27 23:31:17 +0530

    Update help.py

--- Sophia/plugins/help.py
+++ Sophia/plugins/help.py
@@ -15,13 +15,3 @@ async def help(_, message):
         result_id=results.results[0].id
     )
     
-@Sophia.on_message(filters.command(["sudohelp", "shelp"], prefixes=HANDLER))
-async def sudohelp(_, message):
-    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO_USERS_ID:
-        print("")
-    else:
-        return
-    await message.reply(f"ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı\n\n-» .ping - `Get UserBot ping`\n-» .eval - `Run python codes`\n-» .sh - `Run bash commands`\n-» .log - `Get logs`\n-» .shelp - `Get This`\n\n**Powered By**: @Hyper_Speed0™")
-# Warning We just take Reference of text from ZaidUserbot
-# And we don't created that "ıllıllı★ 𝙷𝚎𝚕𝚙 𝙼𝚎𝚗𝚞 ★ıllıllı" its from zaiduserbot Others are reference only
-# If zaid seeing this if have problem contact me t.me/Otazuki I will change it!.
