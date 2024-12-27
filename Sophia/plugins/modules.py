06834dae531430e30aa69c7487789058eca118c4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-27 20:25:49 +0530

    Update modules.py

--- Sophia/plugins/modules.py
+++ Sophia/plugins/modules.py
@@ -1,7 +1,14 @@
-"""from subprocess import getoutput as r
-
+from subprocess import getoutput as r
+import logging 
 a = r("ls Sophia/plugins")
 a = a.split('\n')
+helpNames = []
+help = {}
 for x in a:
   if x.endswith('.py'):
-    exce"""
+    try:
+      exec(f"from Sophia.plugins.{x.split('.py')[0]} import MOD_NAME, MOD_HELP")
+      helpNames += MOD_NAME
+      help[MOD_NAME] = MOD_HELP
+    except: pass
+p(help)
