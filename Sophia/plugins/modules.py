6e7d473918b0e826cc5493effd16233ee627fc61
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-27 23:11:45 +0530

    Update modules.py

--- Sophia/plugins/modules.py
+++ Sophia/plugins/modules.py
@@ -41,6 +41,6 @@ async def showcommands(_, query):
 async def showhelpinfo(_, query):
     help_cmd = str(query.data).replace('help: ', '')
     if help_cmd in help_names:
-        txt = f"⚡ Help for the module: {help_cmd}:\n\n{help_data[help_cmd]}"
+        txt = f"**⚡ Help for the module: {help_cmd}:\n\n{help_data[help_cmd]}**"
         await query.edit_message_text(txt)
         
