63b317a193384c60a98c1857a2ddde80494dee2c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-28 18:24:28 +0530

    Update whisper.py

--- Sophia/Database/whisper.py
+++ Sophia/Database/whisper.py
@@ -1,30 +1,25 @@
 from Sophia import DATABASE
-import asyncio
-import random
 import logging
 
 db = DATABASE["whisper"]
 
 class whisper:
-  async def get(self, id):
-    try:
-      info = await db.find_one({"_id": id})
-      if info and info['msg'] and info['id']:
-        data = {
-          'message': info['msg'],
-          'id': info['id']
-        }
-        return data or {}
-      return {}
-    except Exception as e:
-      logging.error(e)
-      return {}
-  async def add(self, message, id):
-    try:
-      wid = await db.find_one({"_id": 1})
-      if wid: wid = len(wid['ids')+1 or 1
-      else: wid = 1
-      await db.update_one({"_id": 1}, {"$addToSet": {"ids": int(wid)}}, upsert=True)
-      await db.update_one({"_id": int(wid)}, {"$set": {"message": message, "user_id": id}}, upsert=True)
-      return wid
-    except Exception as e: logging.error(e)
+    async def get(self, id):
+        try:
+            info = await db.find_one({"_id": id})
+            if info and 'message' in info and 'user_id' in info:
+                return {'message': info['message'], 'id': info['user_id']}
+            return {}
+        except Exception as e:
+            logging.error(e)
+            return {}
+
+    async def add(self, message, user_id):
+        try:
+            counter = await db.find_one({"_id": 1})
+            wid = counter['wid'] + 1 if counter else 1
+            await db.update_one({"_id": 1}, {"$set": {"wid": wid}}, upsert=True)
+            await db.update_one({"_id": wid}, {"$set": {"message": message, "user_id": user_id}}, upsert=True)
+            return wid
+        except Exception as e:
+            logging.error(e)
