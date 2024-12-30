cafee29b4cbd375d815e7241e5aa27ee6bc7bd7d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-12-30 13:02:28 +0530

    Update Sophia.py

--- Sophia/plugins/Sophia.py
+++ Sophia/plugins/Sophia.py
@@ -3,41 +3,48 @@ from Sophia import *
 from config import OWNER_ID
 from pyrogram import filters
 import asyncio
+import logging
 from pyrogram import enums
 import os
 from datetime import datetime
 from pyrogram import *
 import asyncio
+import traceback
 from Sophia.plugins.modules import a, help_names
 from Sophia.plugins.ping import ping_website
-from pyrogram.types import InlineQueryResultPhoto, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import *
 from pyrogram import __version__
 from Sophia.plugins.play import vcInfo
 
+
 @SophiaBot.on_inline_query(filters.regex('IRLYMANOFR'))
 async def send_btns(_, query):
-  btns = InlineKeyboardMarkup([
-    [
-      InlineKeyboardButton("🆕 What is new?", callback_data=f"SophiaNew"),
-      InlineKeyboardButton("⚙️ Settings", callback_data=f"SophiaPageSettigns")
-    ],
-    [
-      InlineKeyboardButton("🗂️ GitHub", url=f"https://github.com/Otazuki004/SophiaUB"),
-      InlineKeyboardButton("📖 Help", callback_data=f"helppage:1")
-    ],
-    [
-      InlineKeyboardButton("⚕️ Stats ⚕️", callback_data=f"SophiaStats")
-    ],
-    [
-      InlineKeyboardButton("👥 Community", url="https://t.me/Hyper_Speed0")
-    ]
-  ])
-  result = InlineQueryResultPhoto(
-    photo_url="https://i.imgur.com/lgzEDVh.jpeg",
-    caption="Sophia system..",
-    reply_markup=btns
-  )
-  await query.answer([result])
+  try:
+    btns = InlineKeyboardMarkup([
+      [
+        InlineKeyboardButton("🆕 What is new?", callback_data=f"SophiaNew"),
+        InlineKeyboardButton("⚙️ Settings", callback_data=f"SophiaPageSettigns")
+      ],
+      [
+        InlineKeyboardButton("🗂️ GitHub", url=f"https://github.com/Otazuki004/SophiaUB"),
+        InlineKeyboardButton("📖 Help", callback_data=f"helppage:1")
+      ],
+      [
+        InlineKeyboardButton("⚕️ Stats ⚕️", callback_data=f"SophiaStats")
+      ],
+      [
+        InlineKeyboardButton("👥 Community", url="https://t.me/Hyper_Speed0")
+      ]
+    ])
+    result = InlineQueryResultPhoto(
+      photo_url="https://i.imgur.com/lgzEDVh.jpeg",
+      caption="Sophia system..",
+      reply_markup=btns
+    )
+    await query.answer([result])
+  except:
+    e = traceback.format_exc()
+    logging.error(e)
 
 @SophiaBot.on_callback_query(filters.regex('SophiaStats'))
 async def show_stats(_, query):
@@ -75,3 +82,29 @@ async def send_stats(_, message):
         query_id=results.query_id,
         result_id=results.results[0].id
     )
+
+@SophiaBot.on_inline_query(filters.regex('SophiaReportBug'))
+async def send_reportBug(_, query):
+  try:
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton("🪲 Report a bug", user_id=5965055071)]])
+    result = InlineQueryResultArticle(
+      title="Report bug",
+      input_message_content=InputTextMessageContent(
+        f"Click the button below to report bug 🪲"
+      ),
+      reply_markup=btn
+    )
+    await query.answer([result])
+  except:
+    e = traceback.format_exc()
+    logging.error(e)
+
+@Sophia.on_message(filters.command("bug", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def send_rbug(_, message):
+    results = await Sophia.get_inline_bot_results(SophiaBot.me.username, 'SophiaReportBug')
+    await Sophia.send_inline_bot_result(
+        chat_id=message.chat.id,
+        query_id=results.query_id,
+        result_id=results.results[0].id
+    )
+  
