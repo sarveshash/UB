873872d466a9763f5d9454c7a78764b22d3d77a9
Paradopia <paradopia.me@gmail.com>
2024-12-31 17:25:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ OWNER_ID = Sophia.me.id
 
 # SETTINGS 
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
-SUDO_USERS_ID = [6568136732] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
+SUDO_USERS_ID = [7701655337] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False # I will add soon this.
