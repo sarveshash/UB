8c10ef766ee2b4a288a1f59acae472103b465c05
Paradopia <paradopia.me@gmail.com>
2024-12-31 17:49:36 +0530

    Update repo.py

--- Sophia/plugins/repo.py
+++ Sophia/plugins/repo.py
@@ -4,5 +4,5 @@ from pyrogram import *
 @Sophia.on_message(filters.command("repo", prefixes=HANDLER) & filters.user('me'))
 async def repo(_, message):
   if message.reply_to_message:
-    return await message.reply_to_message.reply("https://github.com/Otazuki004/SophiaUB", disable_web_page_preview=True)
-  await message.reply("https://github.com/Otazuki004/SophiaUB", disable_web_page_preview=True)
+    return await message.reply_to_message.reply("https://github.com/FR-AKITO", disable_web_page_preview=True)
+  await message.reply("https://github.com/FR-AKITO", disable_web_page_preview=True)
