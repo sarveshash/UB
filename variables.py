4d17b350d919c88b473d376f65bf0cbb691d9561
Paradopia <paradopia.me@gmail.com>
2024-12-31 17:23:19 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,10 +1,10 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
-VAR_SESSION = ""
+VAR_SESSION = "BQFiozkAI0JnpGyqv2dfnEwKMH5wXB2Doj39fWe3dljsg2NcvqiEy0RkLr3ToR0QlGv5mL7yCtg9fcrt_5YtKZymkdN334YrdPam4rGgGxx1PeeOBys7r9gBaBWd1Xjy_dGvmPwqAyeYEY-8u6LqsirkaOIor3mcQBGVix7CN1A0hus84UTh1esYALuZeS59EgqQPo6wCZ3GSUqz0eroTAIb7o1K8Jiv458U0wvnWK3Af-v5GdaEL9w3fBvhcyibQTfATtSj8n-nrBrzDHiQqXmmtvpDafg9la5wVLwQgcWFl1VfakwRzUCNmcFQAokoRuzc85noTUl9dMgCgNEPifcXDMubHgAAAAHLDe8pAA"
 VAR_API_ID = "10187126"
 VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
-VAR_MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
-VAR_TOKEN = "BQCbcXYAiSQG7bAeyY1tR0zVoyTOhfU7LROEIvP_EGYpCWKq8-OO5HwZYjRIqS7mkrJPj6adatyD-cHS1O-b_q05wiuLrG1Fu508NdFBaB53qIQpTcVcjxl1Mz28Fc6E8qcR45SBxd3RjQ4uyU9rscNKcYTixH0LFmRoR9nBm-hcY6JnZ-FsYbgNUbXMjmMox_wmWIewAwUMSG3GXx7xUdQhlpk1KqjQedBFL2Was3mISV1-jf9uDGOL32gnQVUAg2z5JVUrqzXDbQc4niLgVg3GT1n4PTl27GCu5fpXwepbmAtxm7aTC255agwLHu8XN7CygOocYLY4mbd0UiUEELDIZxmFogAAAAGb5MbNAQ"
-VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
+VAR_MONGO_DB_URI = "mongodb+srv://publicDB:publicDBbyKira@public.twckcqf.mongodb.net/?retryWrites=true&w=majority"
+VAR_TOKEN = "8105141727:AAHWCNbxc2ztcjcPmRE4cp2MCO9q73-h7Io"
+VAR_REPO_URL = "https://github.com/Paradopia/UB/" # Change to your fork repo
 
 # Use "" for strings
